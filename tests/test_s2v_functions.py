55a42c820fb4334393d538046dad07fb1a32efcb
louwenjjr <jorislouwen@hotmail.com>
2020-11-06 11:21:01 +0100

    define path_tests and add assert for matching spectrum ids before and after processing

--- tests/test_s2v_functions.py
+++ tests/test_s2v_functions.py
@@ -6,7 +6,10 @@ import os
 
 def test_post_process_s2v():
     """Test processing and individual spectrum with post_process_s2v"""
+    path_tests = os.path.dirname(__file__)
     testfile = os.path.join(path_tests, "testspectrum_query.json")
     spectrums = json_loader(open(testfile))
     spectrum = post_process_s2v(spectrums[0])
     assert isinstance(spectrum, Spectrum), "Expected output to be Spectrum."
+    assert spectrums[0].metadata["spectrum_id"] == spectrum\
+        .metadata["spectrum_id"]
