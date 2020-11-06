8389465064d31bd57e19486dc9c552fd434ea07b
louwenjjr <jorislouwen@hotmail.com>
2020-11-06 12:47:10 +0100

    added set_spec2vec_defaults

--- ms2query/s2v_functions.py
+++ ms2query/s2v_functions.py
@@ -6,50 +6,58 @@ from matchms.filtering import reduce_to_number_of_peaks
 from matchms.filtering import add_losses
 
 
-def post_process_s2v(spectrum,
-                     mz_from=0,
-                     mz_to=1000,
-                     n_required=10,
-                     ratio_desired=0.5,
-                     intensity_from=0.001,
-                     loss_mz_from=5.0,
-                     loss_mz_to=200.0):
+def set_spec2vec_defaults(**settings):
+    """Set spec2vec default argument values"(where no user input is given)".
+
+    Args
+    ----------
+    **settings: optional dict
+        Change default settings
+    """
+    defaults = {"mz_from": 0,
+                "mz_to": 1000,
+                "n_required": 10,
+                "ratio_desired": 0.5,
+                "intensity_from": 0.001,
+                "loss_mz_from": 5.0,
+                "loss_mz_to": 200.0}
+
+    # Set default parameters or replace by **settings input
+    for key in defaults:
+        if key not in settings:
+            settings[key] = defaults[key]
+    return settings
+
+
+def post_process_s2v(spectrum, **settings):
     """Returns a processed matchms.Spectrum.Spectrum
 
     Args:
     ----------
     spectrum: matchms.Spectrum.Spectrum
         Spectrum to process
-    mz_from: float, optional
-        Set lower threshold for m/z peak positions. Default: 0
-    mz_to: float, optional
-        Set lower threshold for m/z peak positions. Default: 1000
-    n_required: int, optional
-        Number of minimum required peaks. Default: 10
-    ratio_desired: float, optional
-        Set desired ratio between maximum number of peaks and parent mass.
-        Default: 0.5
-    intensity_from: float, optional
-        Set lower threshold for relative peak intensity. Default: 0.001
-    loss_mz_from: float, optional
-        Minimum allowed m/z value for losses. Default: 5.0
-    loss_mz_to: float, optional
-        Maximum allowed m/z value for losses. Default: 200.0
+    **settings: optional dict
+        Change default settings
     """
-    # pylint: disable=too-many-arguments
+    settings = set_spec2vec_defaults(**settings)
     spectrum = normalize_intensities(spectrum)
-    spectrum = select_by_mz(spectrum, mz_from=mz_from, mz_to=mz_to)
+    spectrum = select_by_mz(spectrum, mz_from=settings["mz_from"],
+                            mz_to=settings["mz_to"])
     spectrum = require_minimum_number_of_peaks(spectrum,
-                                               n_required=n_required)
-    spectrum = reduce_to_number_of_peaks(spectrum, n_required=n_required,
-                                         ratio_desired=ratio_desired)
+                                               n_required=settings[
+                                                   "n_required"])
+    spectrum = reduce_to_number_of_peaks(spectrum,
+                                         n_required=settings["n_required"],
+                                         ratio_desired=settings
+                                         ["ratio_desired"])
     if spectrum is None:
         return None
     s_remove_low_peaks = select_by_relative_intensity(spectrum,
-                                                intensity_from=intensity_from)
+                                                      intensity_from=settings
+                                                      ["intensity_from"])
     if len(s_remove_low_peaks.peaks) >= 10:
         spectrum = s_remove_low_peaks
 
-    spectrum = add_losses(spectrum, loss_mz_from=loss_mz_from,
-                          loss_mz_to=loss_mz_to)
+    spectrum = add_losses(spectrum, loss_mz_from=settings["loss_mz_from"],
+                          loss_mz_to=settings["loss_mz_to"])
     return spectrum
