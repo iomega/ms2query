983bdfc7850953c928cae35e50c6d33cf5600ac6
louwenjjr <jorislouwen@hotmail.com>
2020-11-09 17:19:27 +0100

    added function to test matches2network

---
+++ tests/test_networking.py
@@ -0,0 +1,16 @@
+from ms2query.networking import matches2network
+from networkx import Graph
+import os
+
+
+def test_matches2network():
+    """Test matches2network"""
+    path_tests = os.path.dirname(__file__)
+    test_matches = os.path.join(path_tests, "test_found_matches.csv")
+    query_name = "query"
+    test_network = matches2network(query_name, test_matches)
+    assert isinstance(test_network, Graph), "Expected output to be nx.Graph"
+    assert query_name in test_network.nodes,\
+        "Expected query_id to be in the Graph"
+    assert test_network.number_of_edges() == test_matches.shape[0],\
+        "Expected number of edges to be equal to amount of library matches"
