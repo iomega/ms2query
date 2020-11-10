aec5916b6f92e2d729cbcb6a8f564e34d928cb9c
louwenjjr <jorislouwen@hotmail.com>
2020-11-10 11:50:32 +0100

    added test for library_matching

--- tests/test_s2v_functions.py
+++ tests/test_s2v_functions.py
@@ -1,9 +1,12 @@
+import os
+import pandas as pd
+from gensim.models import Word2Vec
 from matchms.Spectrum import Spectrum
 from ms2query.s2v_functions import set_spec2vec_defaults
 from ms2query.s2v_functions import post_process_s2v
 from ms2query.s2v_functions import process_spectrums
+from ms2query.s2v_functions import library_matching
 from ms2query.utils import json_loader
-import os
 from spec2vec import SpectrumDocument
 
 
@@ -37,3 +40,23 @@ def test_process_spectrums():
         "Expected output to be SpectrumDocument."
     assert documents[0]._obj.get("spectrum_id") == spectrums[0]\
         .metadata["spectrum_id"]
+
+
+def test_library_matching():
+    """Test library_matching"""
+    path_tests = os.path.dirname(__file__)
+    testfile_q = os.path.join(path_tests, "testspectrum_query.json")
+    spectrums_q = json_loader(open(testfile_q))
+    testfile_l = os.path.join(path_tests, "testspectrum_library.json")
+    spectrums_l = json_loader(open(testfile_l))
+    documents_q = process_spectrums(spectrums_q)
+    documents_l = process_spectrums(spectrums_l)
+    test_model_file = os.path.join(path_tests,
+                                   "testspectrum_library_model.model")
+    test_model = Word2Vec.load(test_model_file)
+    test_found_matches = library_matching(documents_q, documents_l, test_model,
+                                          allowed_missing_percentage=100)
+    assert isinstance(test_found_matches, pd.DataFrame),\
+        "Expected output to be DataFrame"
+    assert test_found_matches.shape[0] == len(documents_q),\
+        "Expected number of matches to be number of library documents"
