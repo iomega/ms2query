1fabf29cc74171867eecfd3b38cde1ebd0b38190
louwenjjr <jorislouwen@hotmail.com>
2020-11-24 10:37:19 +0100

    correct typo

--- tests/test_app_helpers.py
+++ tests/test_app_helpers.py
@@ -7,7 +7,7 @@ def test_gather_test_json():
     testlib_dict, testlib_list = gather_test_json(test_name)
     out_path = testlib_dict[test_name]
     assert isinstance(testlib_dict, dict), "Expected output to be dict"
-    assert isinstance(testlib_dict, list), "Expected output to be list"
+    assert isinstance(testlib_list, list), "Expected output to be list"
     assert test_name in testlib_dict, "Expected test_name in testlib_dict"
-    assert test_name in testlib_list, "Expected test_name in testlib_dict"
+    assert test_name in testlib_list, "Expected test_name in testlib_list"
     assert isinstance(out_path, str), "Expected output to be str"
