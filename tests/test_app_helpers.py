c9c869834933bcb31d61bc4916aca2a52e6301a7
louwenjjr <jorislouwen@hotmail.com>
2020-11-24 10:32:54 +0100

    initiate test_app_helpers with test_gather_test_json

---
+++ tests/test_app_helpers.py
@@ -0,0 +1,13 @@
+from ms2query.app_helpers import gather_test_json
+
+
+def test_gather_test_json():
+    """Test gather_test_json"""
+    test_name = "testspectrum_library.json"
+    testlib_dict, testlib_list = gather_test_json(test_name)
+    out_path = testlib_dict[test_name]
+    assert isinstance(testlib_dict, dict), "Expected output to be dict"
+    assert isinstance(testlib_dict, list), "Expected output to be list"
+    assert test_name in testlib_dict, "Expected test_name in testlib_dict"
+    assert test_name in testlib_list, "Expected test_name in testlib_dict"
+    assert isinstance(out_path, str), "Expected output to be str"
