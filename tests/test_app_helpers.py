7a82455e244549f8fb13ba02cdeb582f70fa12b8
louwenjjr <jorislouwen@hotmail.com>
2020-11-24 11:23:17 +0100

    added test fro get_zenodo_models_dict

--- tests/test_app_helpers.py
+++ tests/test_app_helpers.py
@@ -1,5 +1,6 @@
 from ms2query.app_helpers import gather_test_json
 from ms2query.app_helpers import gather_zenodo_library
+from ms2query.app_helpers import get_zenodo_models_dict
 
 
 # only functions are tested that do not depend on any streamlit commands
@@ -24,6 +25,20 @@ def test_gather_zenodo_library():
     assert isinstance(lib_dict, dict), "Expected output to be dict"
     assert all(isinstance(lib_dict_key, str)
                for lib_dict_key in lib_dict_keys), "Expected keys to be str"
+    assert isinstance(first_record, tuple), "Expected output to be tuple"
+    assert all(isinstance(first_record[i], str) for i in range(2)),\
+        "Expected first two elements to be str"
+    assert isinstance(first_record[2], int), "Expected third element to be int"
+
+
+def test_get_zenodo_models_dict():
+    """Test get_zenodo_models_dict"""
+    model_dict = get_zenodo_models_dict("downloads")
+    model_dict_keys = list(model_dict.keys())
+    first_record = model_dict[model_dict_keys[0]]
+    assert isinstance(model_dict, dict), "Expected output to be dict"
+    assert all(isinstance(model_dict_key, str) for model_dict_key
+               in model_dict_keys), "Expected keys to be str"
     assert isinstance(first_record, tuple), "Expected output to be tuple"
     assert all(isinstance(first_record[i], str) for i in range(2)),\
         "Expected first two elements to be str"
