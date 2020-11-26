21676b8a0a7e545dc2e1daa79c3a30179495309b
louwenjjr <jorislouwen@hotmail.com>
2020-11-26 17:07:35 +0100

    add test for find_mass_similarity

--- tests/test_ml_functions.py
+++ tests/test_ml_functions.py
@@ -4,6 +4,7 @@ from ms2query.utils import json_loader
 from ms2query.s2v_functions import process_spectrums
 from ms2query.ml_functions import find_basic_info
 from ms2query.ml_functions import transform_num_matches
+from ms2query.ml_functions import find_mass_similarity
 
 
 def test_find_basic_info():
@@ -33,3 +34,20 @@ def test_transform_num_matches():
         "Expected cosine matches to contain floats now"
     assert isinstance(new_test_matches["mod_cosine_matches"].iloc[0], float), \
         "Expected mod cosine matches to contain floats now"
+
+
+def test_find_mass_similarity():
+    """Test find_mass_similarity"""
+    path_tests = os.path.dirname(__file__)
+    testfile_l = os.path.join(path_tests, "testspectrum_library.json")
+    spectrums_l = json_loader(open(testfile_l))
+    documents_l = process_spectrums(spectrums_l)
+    test_matches_file = os.path.join(path_tests, "test_found_matches.csv")
+    test_matches = pd.read_csv(test_matches_file, index_col=0)
+    new_test_matches = find_mass_similarity(test_matches, documents_l, 100.)
+    assert isinstance(new_test_matches, pd.DataFrame), \
+        "Expected output to be df"
+    assert "mass_sim" in new_test_matches.columns, \
+        "Expected mass_sim to be added as a column"
+    assert isinstance(new_test_matches["mass_sim"].iloc[0], float), \
+        "Expected mass_sim to contain floats"
