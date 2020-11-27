1de00545af92d733fc41f74147122c1da7495f6e
louwenjjr <jorislouwen@hotmail.com>
2020-11-27 10:38:13 +0100

    add csv2dict to utils and improve docstring of json_loader

--- ms2query/utils.py
+++ ms2query/utils.py
@@ -1,14 +1,15 @@
 import json
+from typing import Dict, List, TextIO
 from matchms.importing.load_from_json import as_spectrum
 
 
-def json_loader(file):
-    """  
+def json_loader(file: TextIO):
+    """Read open json file with spectra and return
 
-    Parameters
-    ----------
-    file
-        Filename of json to import.
+    Args:
+    -------
+    file:
+        Open file of json to import.
     """
     spectrums = json.load(file)
     if not isinstance(spectrums, list):
@@ -19,3 +20,20 @@ def json_loader(file):
             spectrums[i] = spectrum
 
     return spectrums
+
+
+def csv2dict(file_path: str) -> Dict[str, List[str]]:
+    """Read csv into {first_column: [other_columns]}
+
+    Args:
+    -------
+    file_path
+        Filename of csv to read
+    """
+    csv_dict = {}
+    with open(file_path, "r") as inf:
+        for line in inf:
+            line = line.strip()
+            line = line.split(",")
+            csv_dict[line[0]] = line[1:]
+    return csv_dict
