c09f22e83778c8f7c91800fcd2d81a75e3e88181
Niek de Jonge <n.dejonge@esciencecenter.nl>
2021-01-19 17:33:34 +0100

    Made prototype for storing tanimoto scores in sqlite file

--- ms2query/sqlite.py
+++ ms2query/sqlite.py
@@ -8,7 +8,34 @@ from typing import Dict, List
 import ast
 from matchms.importing.load_from_json import dict2spectrum
 from matchms.Spectrum import Spectrum
+from pandas.io.sql import DataFrame
+import pandas as pd
+import numpy as np
+import os
+import time
 
+
+def make_sqlfile_wrapper(sqlite_file_name: str,
+                         columns_dict: Dict[str, str],
+                         json_spectrum_file_name: str
+                         ):
+    """Creates a new sqlite file, with columns defined in columns_dict
+
+    Args:
+    -------
+    sqlite_file_name:
+        Name of sqlite_file that should be created, if it already exists the
+        table spectra is overwritten.
+    columns_dict:
+        Dictionary with as keys the column names and as values the sql datatype
+    json_spectrum_file_name:
+        File location of the json file that stores the spectra.
+    table_name:
+        Name of the table that is created in the sqlite file,
+        default = "spectra"
+    """
+    create_table_structure(sqlite_file_name, columns_dict)
+    add_spectra_to_database(sqlite_file_name, json_spectrum_file_name)
 
 def create_table_structure(sqlite_file_name: str,
                            columns_dict: Dict[str, str],
@@ -138,3 +165,79 @@ def get_spectra_from_sqlite(sqlite_file_name: str,
     for spectrum in list_of_spectra_dict:
         spectra_list.append(dict2spectrum(spectrum))
     return spectra_list
+
+
+def convert_dataframe_to_sqlite(spectrum_dataframe: pd.DataFrame):
+    connection = sqlite3.connect("dataframe_test_file_without_index.sqlite")
+
+    # Optimal chuncksize: 20000 = 16.5 s, 30000 = 16,3 s
+    spectrum_dataframe.to_sql('tanimoto', connection,
+                              method='multi',
+                              chunksize=30000,
+                              if_exists="append",
+                              index=False)
+    connection.commit()
+    connection.close()
+
+
+def get_inchikey_order(metadata_file: str =
+                          "../downloads/metadata_AllInchikeys14.csv"
+                          ) -> List[str]:
+    """Return list of Inchi14s in same order as in metadata_file
+
+    Args:
+    ------
+    metadata_file:
+        path to metadata file, expected format is csv, with inchikeys in the
+        second column, starting from the second row. Default =
+        "../downloads/metadata_AllInchikeys14.csv"
+    """
+    with open(metadata_file, 'r') as inf:
+        inf.readline()
+        inchi_list = []
+        for line in inf:
+            line = line.strip().split(',')
+            inchi_list.append(line[1])
+    return inchi_list
+
+
+def create_sqlite_table_for_tanimoto_scores():
+
+    #Running for matrix of length 12000
+    inchikey_order = get_inchikey_order()
+    tanimoto_score_matrix = np.load(
+        "../downloads/similarities_AllInchikeys14_daylight2048_jaccard.npy", mmap_mode='r')
+    start_time = time.time()
+    for i in range(0, len(inchikey_order), 2000):
+        part_of_tanimoto_score_matrix = tanimoto_score_matrix[i:i+2000]
+
+        df = pd.DataFrame(part_of_tanimoto_score_matrix,
+                          columns=inchikey_order)
+        df['inchikey1'] = inchikey_order[i:i+2000]
+        df = df.melt(id_vars='inchikey1', var_name='inchikey2', value_name='tanimoto_score', ignore_index=True)
+
+        # add table with dataframe to sqlite file
+        convert_dataframe_to_sqlite(df)
+        print("--- %s seconds ---" % (time.time() - start_time))
+    # df.to_csv("test_tanimoto_scores.csv", index=True, chunksize=100000)
+    return df
+
+
+    # new_np_array = np.array([["","",""]])
+    # for index1, tanimoto_score_array in enumerate(tanimoto_score_matrix):
+    #     for index2 in range(len(tanimoto_score_array)):
+    #         to_add = np.array([[inchikey_order[index1], inchikey_order[index2], tanimoto_score_matrix[index1][index2]]])
+    #         new_np_array = np.append(new_np_array, to_add, axis=0)
+    #     print(index1)
+
+if __name__ == "__main__":
+    # spectrum_dataframe = pd.DataFrame({'name': ['User 1', 'User 2', 'User 3']})
+    # convert_dataframe_to_sqlite(spectrum_dataframe)
+    # data = np.load("../downloads/similarities_AllInchikeys14_daylight2048_jaccard.npy")
+    # my_array = np.array([[1, 2, 3], [2, 1, 4], [3, 4, 1]])
+
+    df = create_sqlite_table_for_tanimoto_scores()
+    # print(df)
+    # print(len(df))
+    # print(df.dtypes)
+    # print(df.memory_usage(deep= True))
