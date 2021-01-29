a2b44f8d7ccaaac97eb49db436585d3d5485a2d7
niekdejonge <76995965+niekdejonge@users.noreply.github.com>
2021-01-29 10:01:49 +0100

    Apply suggestions from code review
    Implemented tqmd to show a progress bar, removed counting the rows and showing the time.
    
    Co-authored-by: Florian Huber <36473328+florian-huber@users.noreply.github.com>

--- ms2query/create_sqlite_database.py
+++ ms2query/create_sqlite_database.py
@@ -323,6 +323,7 @@ def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
                                         col_name_identifier2: str =
                                         'identifier_2',
                                         col_name_score: str = 'tanimoto_score'
+                                        progress_bar: bool = True,
                                         ):
     """Adds tanimoto scores from npy file to sqlite table
 
@@ -348,6 +349,8 @@ def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
     col_name_score:
         Name of the third column of the table, this column will store the
         tanimoto scores. Default = 'tanimoto_score'
+    progress_bar:
+        Show progress bar if set to True. Default is True.
     """
     tanimoto_score_matrix = np.load(
         npy_file_path,
@@ -359,9 +362,9 @@ def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
     for i in range(len(tanimoto_score_matrix[0])):
         list_of_numbers.append(i)
 
-    start_time = time.time()
     # Create a dataframe and add to sqlite table row by row
-    for row_nr, row in enumerate(tanimoto_score_matrix):
+    for row_nr, row in enumerate(tqdm(tanimoto_score_matrix, desc='Adding Tanimoto scores row',
+                                      disable=(not progress_bar))):
         # Remove duplicates
         row = row[:row_nr+1]
         # Transform to pd.Dataframe
@@ -374,12 +377,6 @@ def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
         df.rename(columns={0: col_name_score}, inplace=True)
         # Add dataframe to table in sqlite file
         convert_dataframe_to_sqlite(df, sqlite_file_name)
-
-        if row_nr % 100 == 0:
-            print("The tanimoto scores for " + str(row_nr)
-                  + " inchikeys have been added")
-    print("Adding the tanimoto scores took %s seconds" % (time.time() -
-                                                          start_time))
 
 
 def convert_dataframe_to_sqlite(spectrum_dataframe: pd.DataFrame,
