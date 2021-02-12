d140f1a84b1f0f168b426e0e8757a0bea83f2c67
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-12 18:08:36 +0100

    add s2v processing function

--- ms2query/spectrum_processing.py
+++ ms2query/spectrum_processing.py
@@ -6,7 +6,7 @@ the spectrum peaks (m/z and intensities).
 from typing import Dict, Union
 import numpy as np
 from matchms.typing import SpectrumType
-from matchms.filtering import normalize_intensities, select_by_mz
+from matchms.filtering import normalize_intensities, select_by_mz, select_by_intensity
 
 
 def set_minimal_processing_defaults(**settings: Dict[str, Union[int, float]]) \
@@ -43,21 +43,94 @@ def spectrum_processing_minimal(spectrum: SpectrumType,
     spectrum:
         Spectrum to process
     mz_from
-        Set lower threshold for m/z peak positions. Default is 0.0.
+        Set lower threshold for m/z peak positions. Default is 10.0.
     n_required_below_1000
         Number of minimal required peaks with m/z below 1000.0Da for a spectrum
         to be considered. Spectrums not meeting this criteria will be set to None.
     intensity_from
-        Set lower threshold for peak intensity. Default is 10.0.
+        Set lower threshold for peak intensity. Default is 0.001.
     max_mz_required
         Only peaks <= max_mz_required will be counted to check if spectrum
         contains sufficient peaks to be considered.
     """
     settings = set_minimal_processing_defaults(**settings)
     spectrum = normalize_intensities(spectrum)
+    spectrum = select_by_intensity(spectrum, intensity_from=setting["intensity_from"])
     spectrum = select_by_mz(spectrum, mz_from=settings["mz_from"], mz_to=np.inf)
     spectrum = require_peaks_below_mz(spectrum, n_required=settings["n_required_below_1000"],
                                       max_mz=settings["max_mz_required"])
+    return spectrum
+
+def set_spec2vec_defaults(**settings: Dict[str, Union[int, bool]]) \
+        -> Dict[str, Union[int, bool]]:
+    """Set spec2vec default argument values"(where no user input is given)".
+
+    Args
+    ----------
+    **settings:
+        Change default settings
+    """
+    defaults = {"mz_from": 0,
+                "mz_to": 1000,
+                "n_required": 1,
+                "ratio_desired": 0.5,
+                "intensity_from": 0.001,
+                "n_max": 1000,
+                "loss_mz_from": 5.0,
+                "loss_mz_to": 200.0,
+                "intensity_weighting_power": 0.5,
+                "allowed_missing_percentage": 10.0,
+                }
+
+    # Set default parameters or replace by **settings input
+    for key in defaults:
+        if key not in settings:
+            settings[key] = defaults[key]
+    return settings
+
+
+def spectrum_processing_s2v(spectrum: Spectrum,
+                            **settings: Dict[str, Union[int, bool]]) \
+    -> Union[Spectrum, None]:
+    """Spectrum processing required for computing Spec2Vec scores.
+
+    Args:
+    ----------
+    spectrum:
+        Spectrum to process
+    mz_from
+        Set lower threshold for m/z peak positions. Default is 0.0.
+    mz_to
+        Set upper threshold for m/z peak positions. Default is 1000.0.
+    n_required
+        Number of minimal required peaks for a spectrum to be considered.
+    ratio_desired
+        Number of minimum required peaks. Spectra with fewer peaks will be set
+        to 'None'. Default is 1.
+    intensity_from
+        Set lower threshold for peak intensity. Default is 10.0.
+    n_max
+        Maximum number of peaks to be kept per spectrum. Default is 1000.
+    loss_mz_from
+        Minimum allowed m/z value for losses. Default is 0.0.
+    loss_mz_to
+        Maximum allowed m/z value for losses. Default is 1000.0.
+    intensity_weighting_power
+        Weighing of individual word (=peak) vectors.
+    allowed_missing_percentage
+        Allowed percentage of the entire spectrum that is allowed to not be
+        covered my the pretrained Spec2Vec model.
+    """
+    settings = set_spec2vec_defaults(**settings)
+    spectrum = reduce_to_number_of_peaks(spectrum,
+                                         n_required=settings["n_required"],
+                                         ratio_desired=settings["ratio_desired"],
+                                         n_max=settings["n_max"])
+    if spectrum is None:
+        return None
+
+    spectrum = add_losses(spectrum, loss_mz_from=settings["loss_mz_from"],
+                          loss_mz_to=settings["loss_mz_to"])
     return spectrum
 
 
@@ -66,7 +139,7 @@ def require_peaks_below_mz(spectrum_in: SpectrumType,
                            max_mz: float = 1000.0) -> SpectrumType:
     """Spectrum will be set to None when it has fewer peaks than required.
 
-    Parametersgit 
+    Parametersgit
     ----------
     spectrum_in:
         Input spectrum.
--- tests/test_spectrum_processing.py
+++ tests/test_spectrum_processing.py
@@ -57,8 +57,17 @@ def test_spectrum_processing_minimal_default():
 def test_spectrum_processing_minimal_set_n_required():
     spectrum_in = Spectrum(mz=np.array([5, 110, 220, 330, 440], dtype="float"),
                            intensities=np.array([10, 10, 1, 10, 100], dtype="float"))
-    spectrum = spectrum_processing_minimal(spectrum_in, n_required_below_1000=4)
+    spectrum = spectrum_processing_minimal(spectrum_in, n_required_below_1000=4, max_mz_required=400)
 
     assert np.all(spectrum.peaks.mz == spectrum_in.peaks.mz[1:]), "Expected m/z values to be unchanged"
     assert np.all(spectrum.peaks.intensities == np.array([0.1 , 0.01, 0.1 , 1.  ])), \
         "Expected different, normalized intensities"
+
+
+def test_spectrum_processing_minimal_set_n_required_intensity_from():
+    spectrum_in = Spectrum(mz=np.array([5, 110, 220, 330, 440], dtype="float"),
+                           intensities=np.array([10, 10, 0.1, 10, 101], dtype="float"))
+    spectrum = spectrum_processing_minimal(spectrum_in, n_required_below_1000=4)
+
+    assert None, \
+        "Expected None because 1 peak has m/z > max_mz_required and 1 peak has intensity < intensity_from"
