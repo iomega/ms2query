43ab3ce67e863ae4ce0379ea7dbf79a61ea96a5e
Niek de Jonge <n.dejonge@esciencecenter.nl>
2021-03-12 18:15:29 +0100

    restructured test file directories

--- tests/test_ms2library.py
+++ tests/test_ms2library.py
@@ -157,7 +157,7 @@ def test_get_ms2query_model_prediction():
         'tests/test_files/test_files_ms2library/expected_matches_data.pickle'))
     ms2q_model_file_name = os.path.join(
         os.path.split(os.path.dirname(__file__))[0],
-        'tests/test_files/test_files_ms2library/ms2query_model.hdf5')
+        'tests/test_files/general_test_files/ms2query_model.hdf5')
     result = test_library.get_ms2query_model_prediction(matches_info,
                                                         ms2q_model_file_name)
     expected_result = load_pickled_file(os.path.join(
@@ -174,22 +174,22 @@ def get_test_file_names():
     """Returns file names of the files needed to create Ms2Library object"""
     path_to_tests_dir = os.path.join(
         os.path.split(os.path.dirname(__file__))[0],
-        'tests/test_files/test_files_ms2library')
+        'tests/test_files/')
     sqlite_file_loc = os.path.join(
         path_to_tests_dir,
-        "100_test_spectra.sqlite")
+        "general_test_files/100_test_spectra.sqlite")
     spec2vec_model_file_loc = os.path.join(
         path_to_tests_dir,
-        "100_test_spectra_s2v_model.model")
+        "general_test_files/100_test_spectra_s2v_model.model")
     s2v_pickled_embeddings_file = os.path.join(
         path_to_tests_dir,
-        "100_test_spectra_s2v_embeddings.pickle")
+        "general_test_files/100_test_spectra_s2v_embeddings.pickle")
     ms2ds_model_file_name = os.path.join(
         path_to_tests_dir,
-        "ms2ds_siamese_210301_5000_500_400.hdf5")
+        "general_test_files/ms2ds_siamese_210301_5000_500_400.hdf5")
     ms2ds_embeddings_file_name = os.path.join(
         path_to_tests_dir,
-        "100_test_spectra_ms2ds_embeddings.pickle")
+        "general_test_files/100_test_spectra_ms2ds_embeddings.pickle")
     spectrum_id_column_name = "spectrumid"
     return sqlite_file_loc, spec2vec_model_file_loc, \
         s2v_pickled_embeddings_file, ms2ds_model_file_name, \
