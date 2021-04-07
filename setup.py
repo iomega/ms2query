911d2637c2ae75ee777a9976961ae6fc56cd5bb7
Niek de Jonge <n.dejonge@esciencecenter.nl>
2021-04-07 15:49:07 +0200

    Change version of gensim

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "h5py<3.0.0",
         "tensorflow",
         "ms2deepscore",
+        "gensim=3.8.0",
     ],
     extras_require={"dev": ["bump2version",
                             "isort>=4.2.5,<5",
