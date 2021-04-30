69cf86faaa45d7bab7369fb6e16e00f9756960d5
Niek de Jonge <n.dejonge@esciencecenter.nl>
2021-04-30 12:50:51 +0200

    Changed tests for output_base_filename

--- tests/test_library_files_creator.py
+++ tests/test_library_files_creator.py
@@ -17,13 +17,13 @@ def test_set_settings_correct(path_to_general_test_files):
     """Tests if settings are set correctly"""
     test_create_files = LibraryFilesCreator(os.path.join(
         path_to_general_test_files, '100_test_spectra.pickle'),
-        output_file_sqlite="test_output_name.sqlite",
+        output_base_filename="test_output_name",
         progress_bars=False)
 
     assert test_create_files.settings["output_file_sqlite"] == \
            "test_output_name.sqlite", "Expected different output_file_sqlite"
     assert test_create_files.settings["progress_bars"] is False, \
-        "Expected different setting for progress_bar"
+           "Expected different setting for progress_bar"
     assert test_create_files.settings["spectrum_id_column_name"] == \
            "spectrumid", "Expected different spectrum_id_column_name"
     assert test_create_files.settings["ms2ds_embeddings_file_name"] == \
@@ -40,20 +40,16 @@ def test_set_settings_wrong():
         os.path.split(os.path.dirname(__file__))[0],
         'tests/test_files/test_files_ms2library/100_test_spectra.pickle')
     pytest.raises(AssertionError, LibraryFilesCreator,
-                  pickled_spectra_file_name, "output_filename.sqlite",
+                  pickled_spectra_file_name, "output_filename",
                   not_recognized_attribute="test_value")
 
 
 def test_create_all_library_files(tmp_path, path_to_general_test_files):
     """Tests create_all_library_files"""
-    ms2ds_embeddings_file = os.path.join(tmp_path, "ms2ds_embeddings")
-    s2v_embeddings_file = os.path.join(tmp_path, "s2v_embeddings")
-    sqlite_file = os.path.join(tmp_path, '100_test_spectra.sqlite')
+    base_file_name = os.path.join(tmp_path, '100_test_spectra')
     test_create_files = LibraryFilesCreator(os.path.join(
         path_to_general_test_files, '100_test_spectra.pickle'),
-        sqlite_file,
-        ms2ds_embeddings_file_name=ms2ds_embeddings_file,
-        s2v_embeddings_file_name=s2v_embeddings_file)
+        base_file_name)
     test_create_files.create_all_library_files(
         os.path.join(path_to_general_test_files,
                      '100_test_spectra_tanimoto_scores.pickle'),
@@ -61,15 +57,19 @@ def test_create_all_library_files(tmp_path, path_to_general_test_files):
                      'ms2ds_siamese_210301_5000_500_400.hdf5'),
         os.path.join(path_to_general_test_files,
                      '100_test_spectra_s2v_model.model'))
-    assert os.path.isfile(ms2ds_embeddings_file), \
+
+    expected_ms2ds_emb_file_name = base_file_name + "_ms2ds_embeddings.pickle"
+    expected_s2v_emb_file_name = base_file_name + "_s2v_embeddings.pickle"
+    expected_sqlite_file_name = base_file_name + ".sqlite"
+    assert os.path.isfile(expected_ms2ds_emb_file_name), \
         "Expected ms2ds embeddings file to be created"
-    assert os.path.isfile(s2v_embeddings_file), \
+    assert os.path.isfile(expected_s2v_emb_file_name), \
         "Expected s2v file to be created"
-    assert os.path.isfile(sqlite_file), \
+    assert os.path.isfile(expected_sqlite_file_name), \
         "Expected sqlite file to be created"
     # Test if correct embeddings are stored
-    ms2ds_embeddings = load_pickled_file(ms2ds_embeddings_file)
-    s2v_embeddings = load_pickled_file(s2v_embeddings_file)
+    ms2ds_embeddings = load_pickled_file(expected_ms2ds_emb_file_name)
+    s2v_embeddings = load_pickled_file(expected_s2v_emb_file_name)
     expected_s2v_embeddings = load_pickled_file(os.path.join(
         path_to_general_test_files,
         "100_test_spectra_s2v_embeddings.pickle"))
@@ -81,21 +81,21 @@ def test_create_all_library_files(tmp_path, path_to_general_test_files):
     pd.testing.assert_frame_equal(s2v_embeddings,
                                   expected_s2v_embeddings)
     # Check if sqlite file is stored correctly
-    check_sqlite_files_are_equal(sqlite_file, os.path.join(
+    check_sqlite_files_are_equal(expected_sqlite_file_name, os.path.join(
         path_to_general_test_files, "100_test_spectra.sqlite"))
 
 
 def test_store_ms2ds_embeddings(tmp_path, path_to_general_test_files):
     """Tests store_ms2ds_embeddings"""
-    new_embeddings_file_name = os.path.join(tmp_path,
-                                            "new_test_ms2ds_embeddings.pickle")
+    base_file_name = os.path.join(tmp_path, '100_test_spectra')
     test_create_files = LibraryFilesCreator(os.path.join(
         path_to_general_test_files, '100_test_spectra.pickle'),
-        os.path.join(path_to_general_test_files, '100_test_spectra1.sqlite'),
-        ms2ds_embeddings_file_name=new_embeddings_file_name)
+        base_file_name)
     test_create_files.store_ms2ds_embeddings(os.path.join(
         path_to_general_test_files,
         'ms2ds_siamese_210301_5000_500_400.hdf5'))
+
+    new_embeddings_file_name = base_file_name + "_ms2ds_embeddings.pickle"
     assert os.path.isfile(new_embeddings_file_name), \
         "Expected file to be created"
     # Test if correct embeddings are stored
@@ -108,15 +108,15 @@ def test_store_ms2ds_embeddings(tmp_path, path_to_general_test_files):
 
 def test_store_s2v_embeddings(tmp_path, path_to_general_test_files):
     """Tests store_ms2ds_embeddings"""
-    new_embeddings_file_name = os.path.join(tmp_path,
-                                            "new_test_s2v_embeddings.pickle")
+    base_file_name = os.path.join(tmp_path, '100_test_spectra')
     test_create_files = LibraryFilesCreator(os.path.join(
         path_to_general_test_files, '100_test_spectra.pickle'),
-        os.path.join(path_to_general_test_files, '100_test_spectra2.sqlite'),
-        s2v_embeddings_file_name=new_embeddings_file_name)
+        base_file_name)
     test_create_files.store_s2v_embeddings(os.path.join(
         path_to_general_test_files,
         "100_test_spectra_s2v_model.model"))
+
+    new_embeddings_file_name = base_file_name + "_s2v_embeddings.pickle"
     assert os.path.isfile(new_embeddings_file_name), \
         "Expected file to be created"
     embeddings = load_pickled_file(new_embeddings_file_name)
