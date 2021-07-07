32b1e4088569a7c51b7e592fe9addd67e0c4ec4c
niekdejonge <76995965+niekdejonge@users.noreply.github.com>
2021-07-07 16:04:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ setup(
         "h5py<3.0.0",
         "tensorflow",
         "ms2deepscore",
-        "gensim==3.8.0"
+        "gensim==3.8.0",
+        "pandas==1.2.5"
     ],
     extras_require={"dev": ["bump2version",
                             "isort>=4.2.5,<5",
