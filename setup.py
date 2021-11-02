02211fca874cdad09975b3739ab3b0c4cfb0cf02
niekdejonge <76995965+niekdejonge@users.noreply.github.com>
2021-11-02 12:33:54 +0100

    Define long description content type as text/markdown

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     name="ms2query",
     version=version["__version__"],
     description="Tool to query MS/MS spectra against GNPS library data.",
+    long_description_content_type="text/markdown",
     long_description=readme,
     author="Netherlands eScience Center",
     author_email="",
