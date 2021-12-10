fd59186355d036e9ff803f3f0958c369bedac9a7
Jonge <niek.dejonge@wur.nl>
2021-12-10 17:18:45 +0100

    Added notebooks

--- notebooks/clean_split_and_train_gnps_24_11_21/10_train_ms2query_random_forest.py
+++
@@ -1,6 +0,0 @@
-import os
-from ms2query.utils import load_pickled_file
-
-path_root = os.path.dirname(os.getcwd())
-path_data = os.path.join(os.path.dirname(path_root), "data/gnps_24_11_2021/positive_mode/")
-
