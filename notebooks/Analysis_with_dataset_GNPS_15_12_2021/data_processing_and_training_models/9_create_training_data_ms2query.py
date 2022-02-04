b565aeabd4232575cf7d2b54b16e8d38b07ebb0a
Niek de Jonge <76995965+niekdejonge@users.noreply.github.com>
2022-02-04 15:49:08 +0100

    Apply suggestions from code review
    Change path name in two path names
    
    Co-authored-by: Florian Huber <36473328+florian-huber@users.noreply.github.com>

--- notebooks/Analysis_with_dataset_GNPS_15_12_2021/data_processing_and_training_models/9_create_training_data_ms2query.py
+++ notebooks/Analysis_with_dataset_GNPS_15_12_2021/data_processing_and_training_models/9_create_training_data_ms2query.py
@@ -4,25 +4,27 @@ from ms2query.select_data_for_training_ms2query_nn import DataCollectorForTraini
 file_date = "15_12_2021"
 
 path_data = "C:/Users/jonge094/PycharmProjects/PhD_MS2Query/ms2query/data/libraries_and_models/gnps_15_12_2021/"
+path_library = os.path.join(path_data, "library_GNPS_15_12")
+path_in_between = os.path.join(path_data, "in_between_files")
 
-assert os.path.exists(os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}.sqlite"))
-assert os.path.exists(os.path.join(path_data, "library_GNPS_15_12", f"spec2vec_model_GNPS_{file_date}.model"))
-assert os.path.exists(os.path.join(path_data, "library_GNPS_15_12", f"ms2ds_model_GNPS_{file_date}.hdf5"))
-assert os.path.exists(os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}_s2v_embeddings.pickle"))
-assert os.path.exists(os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}_ms2ds_embeddings.pickle"))
-assert os.path.exists(os.path.join(path_data, "in_between_files", "ms2q_train_spectra.pickle"))
-assert os.path.exists(os.path.join(path_data, "in_between_files", "ms2q_val_spectra.pickle"))
-assert os.path.exists(os.path.join(path_data, "in_between_files", f"GNPS_{file_date}_pos_tanimoto_scores.pickle"))
+assert os.path.exists(os.path.join(path_library, f"library_GNPS_{file_date}.sqlite"))
+assert os.path.exists(os.path.join(path_library, f"spec2vec_model_GNPS_{file_date}.model"))
+assert os.path.exists(os.path.join(path_library, f"ms2ds_model_GNPS_{file_date}.hdf5"))
+assert os.path.exists(os.path.join(path_library, f"library_GNPS_{file_date}_s2v_embeddings.pickle"))
+assert os.path.exists(os.path.join(path_library, f"library_GNPS_{file_date}_ms2ds_embeddings.pickle"))
+assert os.path.exists(os.path.join(path_in_between, "ms2q_train_spectra.pickle"))
+assert os.path.exists(os.path.join(path_in_between, "ms2q_val_spectra.pickle"))
+assert os.path.exists(os.path.join(path_in_between, f"GNPS_{file_date}_pos_tanimoto_scores.pickle"))
 
 collector = DataCollectorForTraining(
     os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}.sqlite"),
-    os.path.join(path_data, "library_GNPS_15_12", f"spec2vec_model_GNPS_{file_date}.model"),
-    os.path.join(path_data, "library_GNPS_15_12", f"ms2ds_model_GNPS_{file_date}.hdf5"),
-    os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}_s2v_embeddings.pickle"),
-    os.path.join(path_data, "library_GNPS_15_12", f"library_GNPS_{file_date}_ms2ds_embeddings.pickle"),
-    os.path.join(path_data, "in_between_files", "ms2q_train_spectra.pickle"),
-    os.path.join(path_data, "in_between_files", "ms2q_val_spectra.pickle"),
-    os.path.join(path_data, "in_between_files", f"GNPS_{file_date}_pos_tanimoto_scores.pickle"),
+    os.path.join(path_library, f"spec2vec_model_GNPS_{file_date}.model"),
+    os.path.join(path_library, f"ms2ds_model_GNPS_{file_date}.hdf5"),
+    os.path.join(path_library, f"library_GNPS_{file_date}_s2v_embeddings.pickle"),
+    os.path.join(path_library, f"library_GNPS_{file_date}_ms2ds_embeddings.pickle"),
+    os.path.join(path_in_between, "ms2q_train_spectra.pickle"),
+    os.path.join(path_in_between, "ms2q_val_spectra.pickle"),
+    os.path.join(path_in_between, f"GNPS_{file_date}_pos_tanimoto_scores.pickle"),
     preselection_cut_off=100)
 
 collector.create_train_and_val_data(os.path.join(path_data,
