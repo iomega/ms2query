bc6bf0a11599aa2238b1ca03ed34f6ba0d5dd11f
florian-huber <florian.huber@hs-duesseldorf.de>
2022-02-04 13:28:54 +0100

    update dependency

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "pandas>=1.2.5",],
     extras_require={':python_version < "3.8"': ["pickle5",],
                     "dev": ["bump2version",
-                            "isort>=4.2.5,<5,>5.1.0",
+                            "isort>=4.2.5,!=5.0.*",
                             "prospector[with_pyroma]",
                             "pytest",
                             "pytest-cov",
