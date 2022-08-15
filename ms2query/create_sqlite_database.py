3697152d8b6daf95a45c3e6d060c5dcfd24246ed
Jonge <niek.dejonge@wur.nl>
2022-08-15 15:13:18 +0200

    removed unnecesarry data from sqlite creator

--- ms2query/create_sqlite_database.py
+++ ms2query/create_sqlite_database.py
@@ -61,79 +61,6 @@ def make_sqlfile_wrapper(sqlite_file_name: str,
                                   progress_bar=progress_bars)
 
 
-def add_tanimoto_scores_to_sqlite(sqlite_file_name: str,
-                                  tanimoto_scores_pickled_dataframe_file: str,
-                                  list_of_spectra: List[Spectrum],
-                                  temporary_tanimoto_file_name: str
-                                  = "temporary_tanimoto_scores",
-                                  progress_bars: bool = True):
-    """Adds tanimoto scores and inchikey14s to sqlite table
-
-    sqlite_file_name:
-        Name of sqlite_file that should be created, if it already exists the
-        tables are added. If the tables in this sqlite file already exist, they
-        will be overwritten.
-    tanimoto_scores_pickled_dataframe_file:
-        A pickled file with tanimoto scores. The column names and indexes are
-        inchikey14s.
-    list_of_spectra:
-        List of spectrum objects
-    temporary_tanimoto_file_name:
-        The file name of a temporary .npy file that is created to memory
-        efficiently read out the tanimoto scores. The file is deleted after
-        finishing.
-    progress_bars:
-        If True progress bars will show the progress of the different steps
-        in the process.
-    """
-    # todo instead of creating a real file and than deleting make a temporary
-    #  file, not yet implemented since numpy memmap is not able to access the
-    #  file in this case.
-
-    temporary_tanimoto_file_name = os.path.join(os.getcwd(),
-                                                temporary_tanimoto_file_name)
-    assert not os.path.exists(temporary_tanimoto_file_name + ".npy"), \
-        "A file already exists with the temporary file name you want to create"
-
-    tanimoto_df = load_pickled_file(tanimoto_scores_pickled_dataframe_file)
-
-    # Get spectra belonging to each inchikey14
-    spectra_belonging_to_inchikey14 = \
-        get_spectra_belonging_to_inchikey14(list_of_spectra)
-
-    inchikeys_with_spectra = list(spectra_belonging_to_inchikey14.keys())
-    inchikeys_with_spectra.sort()
-    # Remove all inchikeys that do not have any matching spectra
-    filtered_tanimoto_df = tanimoto_df.loc[inchikeys_with_spectra][
-        inchikeys_with_spectra]
-
-    inchikeys_order = filtered_tanimoto_df.index
-
-    # Get closest related inchikey14s for each inchikey14
-    closest_related_inchikey14s = \
-        get_closest_related_inchikey14s(filtered_tanimoto_df, inchikeys_order)
-    # Creates a sqlite table containing all tanimoto scores
-    initialize_tanimoto_score_table(sqlite_file_name)
-
-    assert not filtered_tanimoto_df.isnull().values.any(), \
-        "No NaN values were expected in tanimoto scores"
-    if progress_bars:
-        print("Saving tanimoto scores to temporary .npy file")
-    np.save(temporary_tanimoto_file_name, filtered_tanimoto_df.to_numpy())
-
-    add_tanimoto_scores_to_sqlite_table(sqlite_file_name,
-                                        temporary_tanimoto_file_name + ".npy",
-                                        progress_bar=progress_bars)
-    os.remove(temporary_tanimoto_file_name + ".npy")
-    # Creates a table containing the identifiers belonging to each inchikey14
-    # These identifiers correspond to the identifiers in tanimoto_scores
-    create_inchikey_sqlite_table(sqlite_file_name,
-                                 inchikeys_order,
-                                 spectra_belonging_to_inchikey14,
-                                 closest_related_inchikey14s,
-                                 progress_bar=progress_bars)
-
-
 def create_table_structure(sqlite_file_name: str,
                            additional_columns_dict: Dict[str, str] = None,
                            table_name: str = "spectrum_data"):
@@ -171,8 +98,6 @@ def create_table_structure(sqlite_file_name: str,
 
     # Create default combined_columns_dict
     default_columns_dict = {"spectrumid": "INTEGER",
-                            "peaks": "array",
-                            "intensities": "array",
                             "metadata": "TEXT"}
 
     combined_columns_dict = {**default_columns_dict, **additional_columns_dict}
@@ -260,15 +185,11 @@ def add_list_of_spectra_to_sqlite(sqlite_file_name: str,
     for i, spectrum in tqdm(enumerate(list_of_spectra),
                          desc="Adding spectra to sqlite table",
                          disable=not progress_bar):
-        peaks = spectrum.peaks.mz
-        intensities = spectrum.peaks.intensities
         metadata = spectrum.metadata
         spectrumid = i
 
         # Give the value for the default columns
-        value_dict = {'peaks': peaks,
-                      "intensities": intensities,
-                      "metadata": str(metadata),
+        value_dict = {"metadata": str(metadata),
                       "spectrumid": spectrumid}
 
         # Gets the data for addition columns from metadata
@@ -286,10 +207,50 @@ def add_list_of_spectra_to_sqlite(sqlite_file_name: str,
     conn.close()
 
 
+def add_tanimoto_scores_to_sqlite(sqlite_file_name: str,
+                                  tanimoto_scores_pickled_dataframe_file: str,
+                                  list_of_spectra: List[Spectrum],
+                                  progress_bars: bool = True):
+    """Adds tanimoto scores and inchikey14s to sqlite table
+
+    sqlite_file_name:
+        Name of sqlite_file that should be created, if it already exists the
+        tables are added. If the tables in this sqlite file already exist, they
+        will be overwritten.
+    tanimoto_scores_pickled_dataframe_file:
+        A pickled file with tanimoto scores. The column names and indexes are
+        inchikey14s.
+    list_of_spectra:
+        List of spectrum objects
+    progress_bars:
+        If True progress bars will show the progress of the different steps
+        in the process.
+    """
+    tanimoto_df = load_pickled_file(tanimoto_scores_pickled_dataframe_file)
+
+    # Get spectra belonging to each inchikey14
+    spectra_belonging_to_inchikey14 = \
+        get_spectra_belonging_to_inchikey14(list_of_spectra)
+
+    inchikeys_order = tanimoto_df.index
+
+    # Get closest related inchikey14s for each inchikey14
+    closest_related_inchikey14s = \
+        get_closest_related_inchikey14s(tanimoto_df, inchikeys_order)
+
+    # Creates a table containing the identifiers belonging to each inchikey14
+    # These identifiers correspond to the identifiers in tanimoto_scores
+    create_inchikey_sqlite_table(sqlite_file_name,
+                                 inchikeys_order,
+                                 spectra_belonging_to_inchikey14,
+                                 closest_related_inchikey14s,
+                                 progress_bar=progress_bars)
+
+
 def create_inchikey_sqlite_table(
         file_name: str,
         ordered_inchikey_list: List[str],
-        spectra_belonging_to_inchikey: Dict[str, List[str]],
+        spectra_belonging_to_inchikey: Dict[str, List[int]],
         closest_related_inchikeys: Dict[str, List[Tuple[str, float]]],
         progress_bar: bool = True):
     """Creates a table storing the identifiers belonging to the inchikey14s
@@ -391,118 +352,118 @@ def get_closest_related_inchikey14s(tanimoto_scores: pd.DataFrame,
     return closest_related_inchikey14s_dict
 
 
-def initialize_tanimoto_score_table(sqlite_file_name: str,
-                                    table_name: str = 'tanimoto_scores',
-                                    col_name_identifier1: str = 'identifier_1',
-                                    col_name_identifier2: str = 'identifier_2',
-                                    col_name_score: str = 'tanimoto_score'):
-    """Initializes a table for the tanimoto scores.
+# def initialize_tanimoto_score_table(sqlite_file_name: str,
+#                                     table_name: str = 'tanimoto_scores',
+#                                     col_name_identifier1: str = 'identifier_1',
+#                                     col_name_identifier2: str = 'identifier_2',
+#                                     col_name_score: str = 'tanimoto_score'):
+#     """Initializes a table for the tanimoto scores.
+#
+#     Removes the table if it already exists and makes the col_name_identifiers
+#     together a composite key. The datatype of both col_name_identifiers is set
+#     to INTEGER and datatype of col_name_score is set to REAL.
+#
+#     Args:
+#     -------
+#     sqlite_file_name:
+#         Name of sqlite file to which the table should be added.
+#     table_name:
+#         Name of the table in the database that should store the tanimoto
+#         scores. Default = tanimoto_scores.
+#     col_name_identifier1:
+#         Name of the first column of the table, this column will store numbers
+#         that are the identifier of inchikey14s. Default = 'identifier_1'
+#     col_name_identifier2:
+#         Name of the second column of the table, this column will store numbers
+#         that are the identifier of inchikey14s. Default = 'identifier_2'
+#     col_name_score:
+#         Name of the third column of the table, this column will store the
+#         tanimoto scores. Default = 'tanimoto_score'
+#     """
+#     create_table_command = f""";
+#     DROP TABLE IF EXISTS {table_name};
+#     CREATE TABLE {table_name} (
+#         {col_name_identifier1} INTEGER,
+#         {col_name_identifier2} INTEGER,
+#         {col_name_score} REAL,
+#         PRIMARY KEY ({col_name_identifier1}, {col_name_identifier2})
+#         );
+#     """
+#     conn = sqlite3.connect(sqlite_file_name)
+#     cur = conn.cursor()
+#     cur.executescript(create_table_command)
+#     conn.commit()
+#     conn.close()
 
-    Removes the table if it already exists and makes the col_name_identifiers
-    together a composite key. The datatype of both col_name_identifiers is set
-    to INTEGER and datatype of col_name_score is set to REAL.
 
-    Args:
-    -------
-    sqlite_file_name:
-        Name of sqlite file to which the table should be added.
-    table_name:
-        Name of the table in the database that should store the tanimoto
-        scores. Default = tanimoto_scores.
-    col_name_identifier1:
-        Name of the first column of the table, this column will store numbers
-        that are the identifier of inchikey14s. Default = 'identifier_1'
-    col_name_identifier2:
-        Name of the second column of the table, this column will store numbers
-        that are the identifier of inchikey14s. Default = 'identifier_2'
-    col_name_score:
-        Name of the third column of the table, this column will store the
-        tanimoto scores. Default = 'tanimoto_score'
-    """
-    create_table_command = f""";
-    DROP TABLE IF EXISTS {table_name};
-    CREATE TABLE {table_name} (
-        {col_name_identifier1} INTEGER,
-        {col_name_identifier2} INTEGER,
-        {col_name_score} REAL,
-        PRIMARY KEY ({col_name_identifier1}, {col_name_identifier2})
-        );
-    """
-    conn = sqlite3.connect(sqlite_file_name)
-    cur = conn.cursor()
-    cur.executescript(create_table_command)
-    conn.commit()
-    conn.close()
-
-
-def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
-                                        npy_file_path: str,
-                                        col_name_identifier1: str =
-                                        'identifier_1',
-                                        col_name_identifier2: str =
-                                        'identifier_2',
-                                        col_name_score: str = 'tanimoto_score',
-                                        progress_bar: bool = True,
-                                        ):
-    """Adds tanimoto scores from npy file to sqlite table
-
-    The tanimoto scores are added to a sqlite file with in the first and
-    second column the identifiers that correspond to the index of the rows and
-    columns in the .npy file. The third row contains the tanimoto score. All
-    duplicates are removed, before adding to the sqlite table.
-
-    Args:
-    -------
-    sqlite_file_name:
-        Name of sqlite file to which the table should be added.
-    npy_file_path:
-        Path to the numpy file containing the tanimoto scores. The numpy file
-        is expected to contain a matrix filled with floats with nr of columns =
-        nr of rows.
-    col_name_identifier1:
-        Name of the first column of the table, this column will store numbers
-        that are the identifier of inchikey14s. Default = 'identifier_1'
-    col_name_identifier2:
-        Name of the second column of the table, this column will store numbers
-        that are the identifier of inchikey14s. Default = 'identifier_2'
-    col_name_score:
-        Name of the third column of the table, this column will store the
-        tanimoto scores. Default = 'tanimoto_score'
-    progress_bar:
-        Show progress bar if set to True. Default is True.
-    """
-    # pylint: disable=too-many-arguments
-    tanimoto_score_matrix = np.load(
-        npy_file_path,
-        mmap_mode='r')
-
-    # Create a list of consecutive numbers, later used as identifiers for the
-    # inchikey14s
-    list_of_numbers = []
-    for i in range(len(tanimoto_score_matrix[0])):
-        list_of_numbers.append(i+1)
-
-    # Create a dataframe and add to sqlite table row by row
-    for row_nr, row in enumerate(
-            tqdm(tanimoto_score_matrix,
-                 desc="Adding tanimoto scores to sqlite file",
-                 disable=not progress_bar)):
-
-        # Remove duplicates
-        row = row[:row_nr+1]
-        # Transform to pd.Dataframe
-        df = pd.DataFrame(row)
-        # Add columns with identifiers (they correspond to the indexes of the
-        # rows and columns in the npy file
-        df[col_name_identifier1] = np.array(len(row) * [row_nr+1])
-        df[col_name_identifier2] = np.array(list_of_numbers[:len(row)])
-
-        df.rename(columns={0: col_name_score}, inplace=True)
-        # Add dataframe to table in sqlite file
-        convert_dataframe_to_sqlite(df, sqlite_file_name)
-    # del and gc.collect are needed to later delete the file
-    del tanimoto_score_matrix
-    gc.collect()
+# def add_tanimoto_scores_to_sqlite_table(sqlite_file_name: str,
+#                                         npy_file_path: str,
+#                                         col_name_identifier1: str =
+#                                         'identifier_1',
+#                                         col_name_identifier2: str =
+#                                         'identifier_2',
+#                                         col_name_score: str = 'tanimoto_score',
+#                                         progress_bar: bool = True,
+#                                         ):
+#     """Adds tanimoto scores from npy file to sqlite table
+#
+#     The tanimoto scores are added to a sqlite file with in the first and
+#     second column the identifiers that correspond to the index of the rows and
+#     columns in the .npy file. The third row contains the tanimoto score. All
+#     duplicates are removed, before adding to the sqlite table.
+#
+#     Args:
+#     -------
+#     sqlite_file_name:
+#         Name of sqlite file to which the table should be added.
+#     npy_file_path:
+#         Path to the numpy file containing the tanimoto scores. The numpy file
+#         is expected to contain a matrix filled with floats with nr of columns =
+#         nr of rows.
+#     col_name_identifier1:
+#         Name of the first column of the table, this column will store numbers
+#         that are the identifier of inchikey14s. Default = 'identifier_1'
+#     col_name_identifier2:
+#         Name of the second column of the table, this column will store numbers
+#         that are the identifier of inchikey14s. Default = 'identifier_2'
+#     col_name_score:
+#         Name of the third column of the table, this column will store the
+#         tanimoto scores. Default = 'tanimoto_score'
+#     progress_bar:
+#         Show progress bar if set to True. Default is True.
+#     """
+#     # pylint: disable=too-many-arguments
+#     tanimoto_score_matrix = np.load(
+#         npy_file_path,
+#         mmap_mode='r')
+#
+#     # Create a list of consecutive numbers, later used as identifiers for the
+#     # inchikey14s
+#     list_of_numbers = []
+#     for i in range(len(tanimoto_score_matrix[0])):
+#         list_of_numbers.append(i+1)
+#
+#     # Create a dataframe and add to sqlite table row by row
+#     for row_nr, row in enumerate(
+#             tqdm(tanimoto_score_matrix,
+#                  desc="Adding tanimoto scores to sqlite file",
+#                  disable=not progress_bar)):
+#
+#         # Remove duplicates
+#         row = row[:row_nr+1]
+#         # Transform to pd.Dataframe
+#         df = pd.DataFrame(row)
+#         # Add columns with identifiers (they correspond to the indexes of the
+#         # rows and columns in the npy file
+#         df[col_name_identifier1] = np.array(len(row) * [row_nr+1])
+#         df[col_name_identifier2] = np.array(list_of_numbers[:len(row)])
+#
+#         df.rename(columns={0: col_name_score}, inplace=True)
+#         # Add dataframe to table in sqlite file
+#         convert_dataframe_to_sqlite(df, sqlite_file_name)
+#     # del and gc.collect are needed to later delete the file
+#     del tanimoto_score_matrix
+#     gc.collect()
 
 
 def convert_dataframe_to_sqlite(spectrum_dataframe: pd.DataFrame,
