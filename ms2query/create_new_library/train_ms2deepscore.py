7cbfebc85dcc6c027c346d3e751f95e266823c21
Jonge <niek.dejonge@wur.nl>
2022-10-10 15:42:56 +0200

    Added function for training ms2deepscore

--- ms2query/create_new_library/train_ms2deepscore.py
+++ ms2query/create_new_library/train_ms2deepscore.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import pickle
 from typing import List
 from tqdm import tqdm
 import numpy as np
@@ -14,15 +13,14 @@ from tensorflow.keras.callbacks import (  # pylint: disable=import-error
     EarlyStopping, ModelCheckpoint)
 from tensorflow.keras.optimizers import Adam  # pylint: disable=import-error
 from ms2query.create_new_library.create_sqlite_database import select_inchi_for_unique_inchikeys, add_fingerprint
+from ms2query.utils import load_pickled_file
 
 
 def train_ms2ds_model(training_spectra,
                       validation_spectra,
-                      final_model_file_name,
-                      checkpointer_model_file_name,
-                      history_file_name):
-
-    tanimoto_df = calculate_tanimoto_scores(training_spectra+validation_spectra)
+                      tanimoto_df,
+                      output_model_file_name):
+    assert not os.path.isfile(output_model_file_name), "The MS2Deepscore output model file name already exists"
     # Bin training spectra
     spectrum_binner = SpectrumBinner(10000, mz_min=10.0, mz_max=1000.0, peak_scaling=0.5,
                                      allowed_missing_percentage=100.0)
@@ -31,55 +29,44 @@ def train_ms2ds_model(training_spectra,
     # Peaks that do not occur in the training spectra will not be binned in the validaiton spectra.
     binned_spectrums_val = spectrum_binner.transform(validation_spectra)
 
-    dimension = len(spectrum_binner.known_bins)
     same_prob_bins = list(zip(np.linspace(0, 0.9, 10), np.linspace(0.1, 1, 10)))
 
-    selected_inchikeys = np.unique([s.get("inchikey")[:14] for s in training_spectra])
-
-    training_generator = DataGeneratorAllInchikeys(binned_spectrums_training, selected_inchikeys,
-                                                   tanimoto_df,
-                                                   dim=dimension,
-                                                   same_prob_bins=same_prob_bins,
-                                                   num_turns=2,
-                                                   augment_noise_max=10,
-                                                   augment_noise_intensity=0.01)
+    training_generator = DataGeneratorAllInchikeys(
+        binned_spectrums_training,
+        selected_inchikeys=list({s.get("inchikey")[:14] for s in training_spectra}),
+        reference_scores_df=tanimoto_df,
+        dim=len(spectrum_binner.known_bins), # The number of bins created
+        same_prob_bins=same_prob_bins,
+        num_turns=2,
+        augment_noise_max=10,
+        augment_noise_intensity=0.01)
 
     selected_inchikeys = np.unique([s.get("inchikey")[:14] for s in validation_spectra])
 
-    validation_generator = DataGeneratorAllInchikeys(binned_spectrums_val, selected_inchikeys,
-                                                     tanimoto_df,
-                                                     dim=dimension,
-                                                     same_prob_bins=same_prob_bins,
-                                                     num_turns=10,
-                                                     augment_removal_max=0,
-                                                     augment_removal_intensity=0,
-                                                     augment_intensity=0,
-                                                     augment_noise_max=0,
-                                                     use_fixed_set=True)
-    model = SiameseModel(spectrum_binner, base_dims=(500, 500), embedding_dim=200,
-                         dropout_rate=0.2)
+    validation_generator = DataGeneratorAllInchikeys(
+        binned_spectrums_val,
+        selected_inchikeys=list({s.get("inchikey")[:14] for s in binned_spectrums_val}),
+        reference_scores_df=tanimoto_df,
+        dim=len(spectrum_binner.known_bins),  # The number of bins created
+        same_prob_bins=same_prob_bins,
+        num_turns=10, # Number of pairs for each InChiKey14 during each epoch.
+        # To prevent data augmentation
+        augment_removal_max=0, augment_removal_intensity=0, augment_intensity=0, augment_noise_max=0, use_fixed_set=True
+    )
+
+    model = SiameseModel(spectrum_binner, base_dims=(500, 500), embedding_dim=200, dropout_rate=0.2)
+
+    model.compile(loss='mse', optimizer=Adam(lr=0.001), metrics=["mae", tf.keras.metrics.RootMeanSquaredError()])
+
     # Save best model and include early stopping
-    epochs = 150
-    learning_rate = 0.001
-    metrics = ["mae", tf.keras.metrics.RootMeanSquaredError()]
-
-    # Parameters
-    patience_scoring_net = 10
-
-    model.compile(loss='mse', optimizer=Adam(lr=learning_rate), metrics=metrics)
-
-    checkpointer = ModelCheckpoint(filepath=checkpointer_model_file_name, monitor='val_loss', mode="min", verbose=1, save_best_only=True)
-    earlystopper_scoring_net = EarlyStopping(monitor='val_loss', mode="min", patience=patience_scoring_net, verbose=1)
-
-    history = model.model.fit(training_generator, validation_data=validation_generator, epochs=epochs, verbose=1,
-                              callbacks=[earlystopper_scoring_net, checkpointer,])
-
-    # Save history
-    with open(history_file_name, 'wb') as f:
-        pickle.dump(history.history, f)
-
-    model.save(final_model_file_name)
-    # todo make sure the early stopping model is saved.
+    checkpointer = ModelCheckpoint(filepath=output_model_file_name, monitor='val_loss', mode="min", verbose=1, save_best_only=True)
+    earlystopper_scoring_net = EarlyStopping(monitor='val_loss', mode="min", patience=10, verbose=1)
+    # Fit model and save history
+    history = model.model.fit(training_generator, validation_data=validation_generator, epochs=10, verbose=1,
+                              callbacks=[checkpointer, earlystopper_scoring_net])
+    model.load_weights(output_model_file_name)
+    model.save(os.path.join("../data/test_dir/test_library_files_creator", "final_ms2ds_model.hdf5"))
+    return history.history
 
 
 def calculate_tanimoto_scores(list_of_spectra: List[Spectrum]):
@@ -99,7 +86,22 @@ def calculate_tanimoto_scores(list_of_spectra: List[Spectrum]):
     # Specify type and calculate similarities
     similarity_measure = FingerprintSimilarity("jaccard")
     tanimoto_scores = calculate_scores(fingerprint_spectra, fingerprint_spectra,
-                                        similarity_measure,
-                                        is_symmetric=True).scores
+                                       similarity_measure,
+                                       is_symmetric=True).scores
     tanimoto_df = pd.DataFrame(tanimoto_scores, index=unique_inchikeys, columns=unique_inchikeys)
     return tanimoto_df
+
+if __name__ == "__main__":
+    from ms2query.utils import load_matchms_spectrum_objects_from_file
+    data_folder = os.path.join(os.getcwd(), "../../data/")
+    spectra = load_matchms_spectrum_objects_from_file(os.path.join(data_folder,
+                                                                   "libraries_and_models/gnps_15_12_2021/in_between_files/ALL_GNPS_15_12_2021_positive_annotated.pickle"))
+    train_spectra = spectra[:1000]
+    validation_spectra = spectra[-1000:]
+    print("loaded_spectra")
+
+    tanimoto_df = load_pickled_file(os.path.join(data_folder,
+                                                 "libraries_and_models/gnps_15_12_2021/in_between_files/GNPS_15_12_2021_pos_tanimoto_scores.pickle"))
+    print("loaded_df")
+    model_file_name = os.path.join("../data/test_dir/test_library_files_creator", "new_ms2ds_model.hdf5")
+    train_ms2ds_model(train_spectra, validation_spectra, tanimoto_df, model_file_name)
--- tests/test_train_ms2deepscore.py
+++ tests/test_train_ms2deepscore.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
 import pytest
-from ms2query.create_new_library.train_ms2deepscore import calculate_tanimoto_scores
+import h5py
+import keras
+from ms2query.create_new_library.train_ms2deepscore import calculate_tanimoto_scores, train_ms2ds_model
 from ms2query.utils import load_matchms_spectrum_objects_from_file, load_pickled_file
 
 
@@ -20,3 +22,14 @@ def test_calculate_tanimoto_scores(path_to_general_test_files):
                                                           "100_test_spectra_tanimoto_scores.pickle"))
     assert isinstance(tanimoto_df, pd.DataFrame), "Expected a pandas dataframe"
     pd.testing.assert_frame_equal(tanimoto_df, expected_tanimoto_df, check_exact=False, atol=1e-5)
+
+
+def test_train_ms2ds_model(path_to_general_test_files, tmp_path):
+    spectra = load_matchms_spectrum_objects_from_file(os.path.join(path_to_general_test_files, "100_test_spectra.pickle"))
+    tanimoto_df = load_pickled_file("../tests/test_files/general_test_files/100_test_spectra_tanimoto_scores.pickle")
+    model_file_name = os.path.join(tmp_path, "ms2ds_model.hdf5")
+    history = train_ms2ds_model(spectra, spectra, tanimoto_df, model_file_name)
+    assert os.path.isfile(model_file_name), "Expecte ms2ds model to be created and saved"
+    with h5py.File(model_file_name, mode='r') as f:
+        keras_model = keras.models.load_model(f)
+    print(keras_model)
