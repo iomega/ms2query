f431d44d129d22de474f3092b442c1375a986cf3
Jonge <niek.dejonge@wur.nl>
2022-10-17 14:47:03 +0200

    update train ms2deepscore model

--- ms2query/create_new_library/train_ms2deepscore.py
+++ ms2query/create_new_library/train_ms2deepscore.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
-from typing import List
+from typing import List, Dict
 from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
+from matplotlib import pyplot as plt
 from matchms import Spectrum, calculate_scores
 from matchms.similarity import FingerprintSimilarity
 from ms2deepscore import SpectrumBinner
@@ -14,6 +15,7 @@ from tensorflow.keras.callbacks import (  # pylint: disable=import-error
 from tensorflow.keras.optimizers import Adam  # pylint: disable=import-error
 from ms2query.create_new_library.create_sqlite_database import select_inchi_for_unique_inchikeys, add_fingerprint
 from ms2query.utils import load_pickled_file
+from ms2query.create_new_library.clean_and_split_data_for_training import split_spectra_on_inchikeys
 
 
 def train_ms2ds_model(training_spectra,
@@ -41,8 +43,6 @@ def train_ms2ds_model(training_spectra,
         augment_noise_max=10,
         augment_noise_intensity=0.01)
 
-    selected_inchikeys = np.unique([s.get("inchikey")[:14] for s in validation_spectra])
-
     validation_generator = DataGeneratorAllInchikeys(
         binned_spectrums_val,
         selected_inchikeys=list({s.get("inchikey")[:14] for s in binned_spectrums_val}),
@@ -65,7 +65,7 @@ def train_ms2ds_model(training_spectra,
     history = model.model.fit(training_generator, validation_data=validation_generator, epochs=10, verbose=1,
                               callbacks=[checkpointer, earlystopper_scoring_net])
     model.load_weights(output_model_file_name)
-    model.save(os.path.join("../data/test_dir/test_library_files_creator", "final_ms2ds_model.hdf5"))
+    model.save(output_model_file_name)
     return history.history
 
 
@@ -91,17 +91,31 @@ def calculate_tanimoto_scores(list_of_spectra: List[Spectrum]):
     tanimoto_df = pd.DataFrame(tanimoto_scores, index=unique_inchikeys, columns=unique_inchikeys)
     return tanimoto_df
 
+
+def plot_history(history: Dict[str, List[float]]):
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+
+def train_ms2deepscore_wrapper(spectra: List[Spectrum], output_model_file_name, fraction_validation_inchikeys=30):
+    assert not os.path.isfile(output_model_file_name), "The MS2Deepscore output model file name already exists"
+    training_spectra, validation_spectra = split_spectra_on_inchikeys(spectra, fraction_validation_inchikeys)
+    tanimoto_score_df = calculate_tanimoto_scores(spectra)
+    history = train_ms2ds_model(training_spectra, validation_spectra, tanimoto_score_df, output_model_file_name)
+    print(f"The training history is: {history}")
+    plot_history(history)
+
+
 if __name__ == "__main__":
     from ms2query.utils import load_matchms_spectrum_objects_from_file
     data_folder = os.path.join(os.getcwd(), "../../data/")
     spectra = load_matchms_spectrum_objects_from_file(os.path.join(data_folder,
                                                                    "libraries_and_models/gnps_15_12_2021/in_between_files/ALL_GNPS_15_12_2021_positive_annotated.pickle"))
-    train_spectra = spectra[:1000]
-    validation_spectra = spectra[-1000:]
-    print("loaded_spectra")
-
-    tanimoto_df = load_pickled_file(os.path.join(data_folder,
-                                                 "libraries_and_models/gnps_15_12_2021/in_between_files/GNPS_15_12_2021_pos_tanimoto_scores.pickle"))
-    print("loaded_df")
-    model_file_name = os.path.join("../data/test_dir/test_library_files_creator", "new_ms2ds_model.hdf5")
-    train_ms2ds_model(train_spectra, validation_spectra, tanimoto_df, model_file_name)
+    spectra = spectra[:1000]
+    model_file_name = os.path.join("../../data/test_dir/test_library_files_creator", "new_ms2ds_model.hdf5")
+    train_ms2deepscore_wrapper(spectra, model_file_name, 5)
