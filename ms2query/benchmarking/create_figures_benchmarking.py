b8bdf403ad8781a84bd190415fbc6f53d421eded
Jonge <niek.dejonge@wur.nl>
2022-11-15 13:13:34 +0100

    Speed up plotting average tanimoto scores

--- ms2query/benchmarking/create_figures_benchmarking.py
+++ ms2query/benchmarking/create_figures_benchmarking.py
@@ -1,6 +1,10 @@
+import os
+import random
 from typing import List, Tuple, Dict
+from tqdm import tqdm
 import numpy as np
 from matplotlib import pyplot as plt
+from ms2query.utils import load_json_file
 
 
 def select_threshold_for_recall(predictions: List[Tuple[float, float, bool]],
@@ -52,48 +56,90 @@ def compare_tanimoto_score_distribution(predictions_and_scores: Dict[str, List[T
     plt.show()
 
 
+def calculate_recall_vs_tanimoto_scores(selection_criteria_and_tanimoto):
+    percentages_found = []
+    average_tanimoto_score = []
+    # Shuffeling to make sure there is a random order for the matches with the same MS2Query score.
+    random.shuffle(selection_criteria_and_tanimoto)
+    # remove None values
+    not_none_scores = [score for score in selection_criteria_and_tanimoto if score is not None]
+    sorted_scores = sorted(not_none_scores, key=lambda tup: tup[0], reverse=True)
+    sorted_tanimoto_scores = [scores[1] for scores in sorted_scores]
+    for _ in tqdm(range(len(sorted_scores)),
+                  desc="Calculating average Tanimoto score"):
+        percentages_found.append(len(sorted_tanimoto_scores)/len(selection_criteria_and_tanimoto)*100)
+        average_tanimoto_score.append(sum(sorted_tanimoto_scores)/len(sorted_tanimoto_scores))
+        sorted_tanimoto_scores.pop()
+    return percentages_found, average_tanimoto_score
+
+
 def avg_tanimoto_vs_percentage_found(selection_criteria_and_tanimoto,
                                      legend_label,
                                      color_code,
                                      line_style):
     """Plots the average tanimoto vs recall"""
-    percentages_found = []
-    average_tanimoto_score = []
-    sorted_scores = sorted(selection_criteria_and_tanimoto, key=lambda tup: tup[0])
-    for i in range(len(sorted_scores)):
-        selected_scores = [scores[1] for scores in sorted_scores[i:]]
-        percentages_found.append(len(selected_scores)/len(selection_criteria_and_tanimoto)*100)
-        average_tanimoto_score.append(sum(selected_scores)/len(selected_scores))
+    percentages_found, average_tanimoto_score = calculate_recall_vs_tanimoto_scores(selection_criteria_and_tanimoto)
+
     plt.plot(percentages_found, average_tanimoto_score,
              label=legend_label,
              color=color_code,
              linestyle=line_style)
     plt.xlim(100, 10)
-#     plt.ylim(0.4, 1)
+    # plt.ylim(0.4, 1)
     plt.xlabel("Recall (%)")
     plt.ylabel("Average Tanimoto score")
     plt.suptitle("Analogues test set")
     plt.legend(loc="lower right",
                title="Select on:")
 
-#
-# def add_colour_and_linetype_labels(test_results):
-#     pass
+
+def load_results_from_folder(test_results_folder: str):
+    dict_with_results = {}
+    dict_with_results["MS2Query"] = load_json_file(os.path.join(test_results_folder, "ms2query_test_results.json"))
+    dict_with_results["MS2Deepscore"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_all.json"))
+    dict_with_results["MS2Deepscore 100 Da"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_100_Da.json"))
+    dict_with_results["Cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "cosine_score_100_da_test_results.json"))
+    dict_with_results["Modified cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "modified_cosine_score_100_Da_test_results.json"))
+    dict_with_results["Optimal"] = load_json_file(os.path.join(test_results_folder, "optimal_results.json"))
+    dict_with_results["Random"] = load_json_file(os.path.join(test_results_folder, "random_results.json"))
+    return dict_with_results
+
+
+def plot_results(dict_with_results):
+    colours = {"MS2Query": ('#3C5289', "-"),
+               "MS2Deepscore": ('#49C16D', "-"),
+               "MS2Deepscore 100 Da": ('#49C16D', "-"),
+               "Cosine score 100 Da": ('#F5E21D', "-"),
+               "Modified cosine score 100 Da": ('#F5E21D', "-"),
+               "Optimal": ('#000000', "--"),
+               "Random": ('#808080', "--")}
+    for results_name, scores in dict_with_results.items():
+        avg_tanimoto_vs_percentage_found(scores, results_name, colours[results_name][0], colours[results_name][1])
+    plt.show()
+
+
+def combine_all_test_results():
+    base_directory = "../../data/libraries_and_models/gnps_01_11_2022/20_fold_splits/"
+    results_dict = {"MS2Query": [],
+                    "MS2Deepscore": [],
+                    "MS2Deepscore 100 Da": [],
+                    "Cosine score 100 Da": [],
+                    "Modified cosine score 100 Da": [],
+                    "Optimal": [],
+                    "Random": []}
+    for i in range(20):
+        test_results_directory = os.path.join(base_directory, f"test_split_{i}", "test_results")
+        try:
+            results = load_results_from_folder(test_results_directory)
+            for key in results_dict:
+                results_dict[key] += results[key]
+        except:
+            print(f"not all test results were generated for : {test_results_directory}")
+    return results_dict
 
 
 if __name__ == "__main__":
-    from ms2query.utils import load_json_file
-    # test_results = load_json_file("../../data/test_dir/test_generate_test_results/test_results.json")
-    # ms2query_test_results = test_results["ms2query_results"]
-    # cosine_test_results = test_results["cosine_results"]
-    # modified_cosine_test_results = test_results["modified_cosine_results"]
-    # ms2deepscore_test_results = test_results["ms2ds_results"]
-    ms2query_test_results = load_json_file("../../data/test_dir/train_and_test_library/test_results_ms2query.json")
-    avg_tanimoto_vs_percentage_found(ms2query_test_results, "MS2Query", '#3C5289', "-")
-    # avg_tanimoto_vs_percentage_found(ms2deepscore_test_results, "MS2Deepscore", '#49C16D', "-")
-    # avg_tanimoto_vs_percentage_found(modified_cosine_test_results, "Modified Cosine", '#F5E21D', "-")
-    plt.show()
-    # avg_tanimoto_vs_percentage_found(optimal_results, "Optimal", '#000000', "--")
-    # avg_tanimoto_vs_percentage_found(random_results, "Random", '#808080', "--")
-    # compare_tanimoto_score_distribution(test_results,
-    #                                     0.7, len(list(test_results.values())[0]))
+    # test_results_folder = "../../data/test_dir/train_and_test_library/test_results/"
+    # test_results_folder = "../../data/libraries_and_models/gnps_01_11_2022/20_fold_splits/test_split_0/test_results/"
+    dict_with_results = combine_all_test_results()
+    plot_results(dict_with_results)
