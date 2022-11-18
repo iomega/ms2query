12a0521f50560dbe11e7ab32d7fe3911714e9f94
Jonge <niek.dejonge@wur.nl>
2022-11-18 11:35:41 +0100

    Remove library files creator from init to prevent dependency on rdkit

--- ms2query/__init__.py
+++ ms2query/__init__.py
@@ -1,6 +1,5 @@
 import logging
 from .__version__ import __version__
-from ms2query.create_new_library.library_files_creator import LibraryFilesCreator
 from .ms2library import MS2Library
 from .results_table import ResultsTable
 
@@ -10,7 +9,6 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 __author__ = "Netherlands eScience Center"
 __all__ = [
     "__version__",
-    "LibraryFilesCreator",
     "MS2Library",
     "ResultsTable",
 ]
