7336cb0ed5d796770befd334fd413de0b432658c
Jonge <niek.dejonge@wur.nl>
2022-11-29 10:15:33 +0100

    Add plot exact matches

--- ms2query/benchmarking/create_accuracy_vs_recall_plot.py
+++ ms2query/benchmarking/create_accuracy_vs_recall_plot.py
@@ -4,7 +4,7 @@ test results for benchmarking MS2Query against other tools.
 """
 import os
 import random
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from tqdm import tqdm
 import numpy as np
 from matplotlib import pyplot as plt
@@ -37,7 +37,7 @@ def plot_all_with_standard_deviation(means_and_standars_deviation,
                          )
 
     plt.xlim(100, 0)
-    plt.ylim(0, 1.05)
+    # plt.ylim(0, 100)
     plt.xlabel("Recall (%)")
     plt.ylabel("Average Tanimoto score")
     plt.suptitle("Analogues test set")
@@ -49,16 +49,17 @@ def plot_all_with_standard_deviation(means_and_standars_deviation,
         plt.savefig(save_figure_file_name, format="svg")
 
 
-def calculate_all_means_and_standard_deviation(dict_with_results):
+def calculate_all_means_and_standard_deviation(dict_with_results,
+                                               exact_matches: bool = False):
     means_and_standard_deviation = {}
     for results_name, scores in dict_with_results.items():
-        binned_percentages, means, standard_deviations = calculate_means_and_standard_deviation(scores)
+        binned_percentages, means, standard_deviations = calculate_means_and_standard_deviation(scores,
+                                                                                                exact_matches=exact_matches)
         means_and_standard_deviation[results_name] = (binned_percentages, means, standard_deviations)
     return means_and_standard_deviation
 
 
-def load_all_test_results(nr_of_test_results):
-    base_directory = "../../data/libraries_and_models/gnps_01_11_2022/20_fold_splits/"
+def load_all_test_results(nr_of_test_results, base_directory):
     results_dict = {"MS2Query": [],
                     "MS2Deepscore": [],
                     "MS2Deepscore 100 Da": [],
@@ -77,7 +78,7 @@ def load_all_test_results(nr_of_test_results):
     return results_dict
 
 
-def load_results_from_folder(test_results_folder: str):
+def load_results_from_folder(test_results_folder: str) -> Dict[str, List[Tuple[float, float, bool]]]:
     dict_with_results = {}
     dict_with_results["MS2Query"] = load_json_file(os.path.join(test_results_folder, "ms2query_test_results.json"))
     dict_with_results["MS2Deepscore"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_all.json"))
@@ -89,13 +90,33 @@ def load_results_from_folder(test_results_folder: str):
     return dict_with_results
 
 
+def calculate_recall_vs_exact_matches(selection_criteria_and_tanimoto):
+    percentages_found = []
+    percentage_exact_match = []
+    # Shuffeling to make sure there is a random order for the matches with the same MS2Query score.
+    random.shuffle(selection_criteria_and_tanimoto)
+    # remove None values
+    not_none_scores = [score for score in selection_criteria_and_tanimoto if score is not None]
+    sorted_scores = sorted(not_none_scores, key=lambda tup: tup[0], reverse=True)
+    sorted_exact_match_bool = [scores[2] for scores in sorted_scores]
+    for _ in range(len(sorted_scores)):
+        percentages_found.append(len(sorted_exact_match_bool)/len(selection_criteria_and_tanimoto)*100)
+        percentage_exact_match.append(sum(sorted_exact_match_bool)/len(sorted_exact_match_bool)*100)
+        sorted_exact_match_bool.pop()
+    return percentages_found, percentage_exact_match
+
+
 def calculate_means_and_standard_deviation(k_fold_results: List[List[Tuple[float, float, bool]]],
-                                           step_size=0.1):
+                                           step_size=0.1,
+                                           exact_matches: bool = False):
     accuracies = []
     binned_percentages = np.arange(0 + step_size/2, 100 + step_size/2, step_size)
     for results in tqdm(k_fold_results,
                         desc="Calculating recall, mean and standard deviation"):
-        percentages_found, average_tanimoto = calculate_recall_vs_tanimoto_scores(results)
+        if exact_matches:
+            percentages_found, average_tanimoto = calculate_recall_vs_exact_matches(results)
+        else:
+            percentages_found, average_tanimoto = calculate_recall_vs_tanimoto_scores(results)
         binned_accuracies = bin_percentages(percentages_found, average_tanimoto, step_size)
         accuracies.append(binned_accuracies)
     accuracies_matrix = np.vstack(accuracies)
@@ -136,12 +157,12 @@ def calculate_recall_vs_tanimoto_scores(selection_criteria_and_tanimoto):
 
 
 if __name__ == "__main__":
-    test_results_folder = "../../data/libraries_and_models/gnps_01_11_2022/20_fold_splits/"
-    dict_with_results = load_all_test_results(20)
-    means_and_standard_deviation = calculate_all_means_and_standard_deviation(dict_with_results)
-    # means_and_standard_deviation = load_pickled_file(os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
+    test_results_folder = "../../data/libraries_and_models/gnps_01_11_2022/negative_mode/exact_matches_test_sets/"
+    # dict_with_results = load_all_test_results(20, test_results_folder)
+    # means_and_standard_deviation = calculate_all_means_and_standard_deviation(dict_with_results, exact_matches=True)
+    means_and_standard_deviation = load_pickled_file(os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
     plot_all_with_standard_deviation(means_and_standard_deviation,
-                                     save_figure_file_name=os.path.join(test_results_folder, "recall_vs_accuracy_20_fold.svg")
+                                     save_figure_file_name=os.path.join(test_results_folder, "recall_vs_accuracy_neg_exact_matches.svg")
                                      )
-    from ms2query.utils import save_pickled_file
-    save_pickled_file(means_and_standard_deviation, os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
+    # from ms2query.utils import save_pickled_file
+    # save_pickled_file(means_and_standard_deviation, os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
