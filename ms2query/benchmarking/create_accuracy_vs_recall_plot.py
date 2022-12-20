48d4d80a49fc0922e9e63d76ddf1dba50869e1fb
Jonge <niek.dejonge@wur.nl>
2022-12-20 11:50:15 +0100

    made creation of plots more automated

--- ms2query/benchmarking/create_accuracy_vs_recall_plot.py
+++ ms2query/benchmarking/create_accuracy_vs_recall_plot.py
@@ -9,17 +9,56 @@ from tqdm import tqdm
 import numpy as np
 from matplotlib import pyplot as plt
 
-from ms2query.utils import load_json_file, load_pickled_file
+from ms2query.utils import load_json_file, load_pickled_file, save_pickled_file
 
 
 def plot_all_with_standard_deviation(means_and_standars_deviation,
-                                     save_figure_file_name=None):
+                                     save_figure_file_name=None,
+                                     ):
     colours = {
         "MS2Query": ('#3C5289', "-"),
-        # "MS2Deepscore": ('#013220', "-"),
         "MS2Deepscore 100 Da": ('#49C16D', "-"),
-        # "Cosine score 100 Da": ('#FFA500', "-"),
+        "Cosine score 100 Da": ('#FFA500', "-"),
         "Modified cosine score 100 Da": ('#F5E21D', "-"),
+        # "MS2Deepscore 0.25 Da": ('#49C16D', "-"),
+        # "Cosine score 0.25 Da": ('#FFA500', "-"),
+        # "Modified cosine score 0.25 Da": ('#F5E21D', "-"),
+        "Optimal": ('#000000', "--"),
+        "Random": ('#808080', "--")
+    }
+
+    for test_type, colour_and_line_type in colours.items():
+        colour = colour_and_line_type[0]
+        line_type = colour_and_line_type[1]
+        binned_percentages, means, standard_deviations = means_and_standars_deviation[test_type]
+        plt.plot(binned_percentages, means,
+                 label=test_type,
+                 color=colour,
+                 linestyle=line_type)
+        plt.fill_between(binned_percentages, means - standard_deviations, means + standard_deviations, alpha=0.3,
+                         color=colour,
+                         )
+
+    plt.xlim(100, 0)
+    plt.ylim(0, 1.05)
+    plt.xlabel("Recall (%)")
+    plt.ylabel("Average Tanimoto score")
+    plt.suptitle("Analogues test set")
+    plt.legend(loc="lower left")
+    if save_figure_file_name is None:
+        plt.show()
+    else:
+        assert not os.path.isfile(save_figure_file_name)
+        plt.savefig(save_figure_file_name, format="svg")
+
+
+def plot_exact_matches_results_with_standard_deviation(means_and_standars_deviation,
+                                                       save_figure_file_name=None):
+    colours = {
+        "MS2Query": ('#3C5289', "-"),
+        "MS2Deepscore 0.25 Da": ('#49C16D', "-"),
+        "Cosine score 0.25 Da": ('#FFA500', "-"),
+        "Modified cosine score 0.25 Da": ('#F5E21D', "-"),
         "Optimal": ('#000000', "--"),
         "Random": ('#808080', "--")
     }
@@ -39,8 +78,8 @@ def plot_all_with_standard_deviation(means_and_standars_deviation,
     plt.xlim(100, 0)
     # plt.ylim(0, 100)
     plt.xlabel("Recall (%)")
-    plt.ylabel("Average Tanimoto score")
-    plt.suptitle("Analogues test set")
+    plt.ylabel("True matches (%)")
+    plt.suptitle("Exact matches test set")
     plt.legend(loc="lower left")
     if save_figure_file_name is None:
         plt.show()
@@ -59,32 +98,42 @@ def calculate_all_means_and_standard_deviation(dict_with_results,
     return means_and_standard_deviation
 
 
-def load_all_test_results(nr_of_test_results, base_directory):
-    results_dict = {"MS2Query": [],
-                    "MS2Deepscore": [],
-                    "MS2Deepscore 100 Da": [],
-                    "Cosine score 100 Da": [],
-                    "Modified cosine score 100 Da": [],
-                    "Optimal": [],
-                    "Random": []}
+def load_all_test_results(nr_of_test_results, base_directory, exact_match=False):
+
+    results_dict = {}
     for i in range(nr_of_test_results):
         test_results_directory = os.path.join(base_directory, f"test_split_{i}", "test_results")
         try:
-            results = load_results_from_folder(test_results_directory)
-            for key in results_dict:  # pylint: disable=consider-using-dict-items
-                results_dict[key].append(results[key])
+            results = load_results_from_folder(test_results_directory,
+                                               exact_matches=exact_match)
+            for key in results:  # pylint: disable=consider-using-dict-items
+                if key in list(results_dict.keys()):
+                    results_dict[key].append(results[key])
+                else:
+                    results_dict[key] = [results[key]]
         except IOError:
             print(f"not all test results were generated for : {test_results_directory}")
     return results_dict
 
 
-def load_results_from_folder(test_results_folder: str) -> Dict[str, List[Tuple[float, float, bool]]]:
+def load_results_from_folder(test_results_folder: str,
+                             exact_matches = False) -> Dict[str, List[Tuple[float, float, bool]]]:
     dict_with_results = {}
+    if not exact_matches:
+        dict_with_results["Cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "cosine_score_100_da_test_results.json"))
+        dict_with_results["MS2Deepscore 100 Da"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_100_Da.json"))
+        dict_with_results["Modified cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "modified_cosine_score_100_Da_test_results.json"))
+    else:
+        dict_with_results["MS2Deepscore 0.25 Da"] = load_json_file(
+            os.path.join(test_results_folder, "ms2deepscore_test_results_0_25_Da.json"))
+        dict_with_results["Cosine score 0.25 Da"] = load_json_file(
+            os.path.join(test_results_folder, "cosine_score_0_25_da_test_results.json"))
+        dict_with_results["Modified cosine score 0.25 Da"] = load_json_file(
+            os.path.join(test_results_folder, "modified_cosine_score_0_25_Da_test_results.json"))
+
     dict_with_results["MS2Query"] = load_json_file(os.path.join(test_results_folder, "ms2query_test_results.json"))
-    dict_with_results["MS2Deepscore"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_all.json"))
-    dict_with_results["MS2Deepscore 100 Da"] = load_json_file(os.path.join(test_results_folder, "ms2deepscore_test_results_100_Da.json"))
-    dict_with_results["Cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "cosine_score_100_da_test_results.json"))
-    dict_with_results["Modified cosine score 100 Da"] = load_json_file(os.path.join(test_results_folder, "modified_cosine_score_100_Da_test_results.json"))
+    dict_with_results["MS2Deepscore"] = load_json_file(
+        os.path.join(test_results_folder, "ms2deepscore_test_results_all.json"))
     dict_with_results["Optimal"] = load_json_file(os.path.join(test_results_folder, "optimal_results.json"))
     dict_with_results["Random"] = load_json_file(os.path.join(test_results_folder, "random_results.json"))
     return dict_with_results
@@ -135,7 +184,9 @@ def bin_percentages(percentages, accuracies, step_size):
         for i, percentage_found in enumerate(percentages):
             if lower_end_of_bin < percentage_found <= upper_end_of_bin:
                 accuracies_in_bin.append(accuracies[i])
-        average = sum(accuracies_in_bin)/len(accuracies_in_bin)
+
+        if len(accuracies_in_bin) != 0:
+            average = sum(accuracies_in_bin)/len(accuracies_in_bin)
         binned_accuracies.append(average)
     return binned_accuracies
 
@@ -156,13 +207,65 @@ def calculate_recall_vs_tanimoto_scores(selection_criteria_and_tanimoto):
     return percentages_found, average_tanimoto_score
 
 
+def create_plot(exact_matches,
+                positive,
+                recalculate_means,
+                save_figure,
+                base_folder):
+    if positive:
+        if exact_matches:
+            test_results_folder = os.path.join(base_folder, "positive_mode/exact_matches_test_sets_splits")
+        else:
+            test_results_folder = os.path.join(base_folder, "positive_mode/analogue_test_sets_splits")
+    else:
+        if not exact_matches:
+            test_results_folder = os.path.join(base_folder, "negative_mode/analogue_test_sets_splits")
+        else:
+            test_results_folder = os.path.join(base_folder, "negative_mode/exact_matches_test_sets_splits")
+
+    if exact_matches:
+        extra_file_name = "_exact_matches"
+    else:
+        extra_file_name = ""
+
+    if recalculate_means:
+        dict_with_results = load_all_test_results(20, test_results_folder, exact_match=exact_matches)
+        means_and_standard_deviation = calculate_all_means_and_standard_deviation(dict_with_results, exact_matches=exact_matches)
+        save_pickled_file(means_and_standard_deviation,
+                          os.path.join(test_results_folder, f"means_and_standard_deviations_20_fold{extra_file_name}.pickle"))
+    else:
+        means_and_standard_deviation = load_pickled_file(os.path.join(test_results_folder, f"means_and_standard_deviations_20_fold{extra_file_name}.pickle"))
+
+    if exact_matches:
+        optimal_results = means_and_standard_deviation["Optimal"]
+        new_optimal_results = (optimal_results[0], np.full(1000, 100), np.full(1000, 0))
+        means_and_standard_deviation["Optimal"] = new_optimal_results
+    if not exact_matches:
+        if save_figure:
+            plot_all_with_standard_deviation(means_and_standard_deviation,
+                                             save_figure_file_name=os.path.join(test_results_folder,
+                                                                                "recall_vs_accuracy_final.svg")
+                                             )
+        else:
+            plot_all_with_standard_deviation(means_and_standard_deviation)
+    else:
+        if save_figure:
+            plot_exact_matches_results_with_standard_deviation(means_and_standard_deviation,
+                                                               save_figure_file_name=os.path.join(test_results_folder,
+                                                                                                  "recall_vs_accuracy_final.svg")
+                                             )
+        else:
+            plot_exact_matches_results_with_standard_deviation(means_and_standard_deviation)
+
+
 if __name__ == "__main__":
-    test_results_folder = "../../data/libraries_and_models/gnps_01_11_2022/negative_mode/exact_matches_test_sets/"
-    # dict_with_results = load_all_test_results(20, test_results_folder)
-    # means_and_standard_deviation = calculate_all_means_and_standard_deviation(dict_with_results, exact_matches=True)
-    means_and_standard_deviation = load_pickled_file(os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
-    plot_all_with_standard_deviation(means_and_standard_deviation,
-                                     save_figure_file_name=os.path.join(test_results_folder, "recall_vs_accuracy_neg_exact_matches.svg")
-                                     )
-    # from ms2query.utils import save_pickled_file
-    # save_pickled_file(means_and_standard_deviation, os.path.join(test_results_folder, "means_and_standard_deviations_20_fold.pickle"))
+    # The folder where the benchmarking results are stored.
+    # These can be downloaded from https://zenodo.org/record/7427094
+    base_folder = "../../data/libraries_and_models/gnps_01_11_2022"
+
+    create_plot(exact_matches=True,
+                positive=False,
+                recalculate_means=True,
+                save_figure=False,
+                base_folder=base_folder
+                )
