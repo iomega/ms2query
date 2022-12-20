15fa4ec89ee61311b4282d0d84f512afb7f16ba7
Jonge <niek.dejonge@wur.nl>
2022-12-20 11:54:17 +0100

    Added option for exact matches test results creation

--- ms2query/benchmarking/k_fold_cross_validation.py
+++ ms2query/benchmarking/k_fold_cross_validation.py
@@ -10,7 +10,7 @@ from matchms import Spectrum
 from ms2query.create_new_library.split_data_for_training import split_spectra_in_random_inchikey_sets, select_spectra_per_unique_inchikey
 from ms2query.utils import load_matchms_spectrum_objects_from_file, save_pickled_file
 from ms2query.create_new_library.train_models import train_all_models
-from ms2query.benchmarking.collect_test_data_results import generate_test_results
+from ms2query.benchmarking.collect_test_data_results import generate_test_results, generate_exact_matches_test_results
 from ms2query.clean_and_filter_spectra import clean_normalize_and_split_annotated_spectra
 from ms2query.ms2library import create_library_object_from_one_dir
 
@@ -30,6 +30,7 @@ def split_and_store_annotated_unannotated(spectrum_file_name,
     save_pickled_file(annotated_spectra,
                       os.path.join(output_folder, "annotated_training_spectra.pickle"))
     return annotated_spectra, unannotated_spectra
+
 
 def split_k_fold_cross_validation_analogue_test_set(annotated_spectra: List[Spectrum],
                                                     k: int,
@@ -101,41 +102,7 @@ def train_models_and_test_result_from_k_fold_folder(k_fold_split_folder:str,
 
     # Generate test results
     ms2library = create_library_object_from_one_dir(models_folder)
-    generate_test_results(ms2library,
-                          annotated_training_spectra,
-                          test_spectra,
-                          test_results_folder)
-
-
-if __name__ == "__main__":
-    base_dir = "../../data/libraries_and_models/gnps_01_11_2022/negative_mode"
-    # # annotated_spectra, unannotated_spectra = split_and_store_annotated_unannotated(
-    # #     os.path.join(base_dir, "ALL_GNPS_NO_PROPOGATED.mgf"),
-    # #     "negative",
-    # #     os.path.join(base_dir, "negative_mode"))
-    # annotated_spectra = load_matchms_spectrum_objects_from_file(
-    #     os.path.join(base_dir, "annotated_training_spectra.pickle"))
-    # # split_k_fold_cross_validation_analogue_test_set(annotated_spectra,
-    # #                                                 20,
-    # #                                                 os.path.join(base_dir, "negative_mode/analogue_test_sets/"))
-    # split_k_fold_cross_validation_exact_match_test_set(annotated_spectra,
-    #                                                    20,
-    #                                                    os.path.join(base_dir, "exact_matches_test_sets_1"))
-
-    k_fold_split_number = sys.argv[1]
-    # train_models_and_test_result_from_k_fold_folder("../../data/libraries_and_models/gnps_01_11_2022/negative_mode/exact_matches_test_sets_1/",
-    #                                                 k_fold_split_number)
-    train_models_and_test_result_from_k_fold_folder("../../data/libraries_and_models/gnps_01_11_2022/exact_matches_split_1/",
-                                                    k_fold_split_number)
-    # spectra = load_matchms_spectrum_objects_from_file("../../data/libraries_and_models/gnps_01_11_2022/ALL_GNPS_NO_PROPOGATED.mgf")
-    # split_k_fold_cross_validation_exact_match_test_set(spectra,
-    #                                                    k=20,
-    #                                                    ion_mode="positive",
-    #                                                    output_folder="../../data/libraries_and_models/gnps_01_11_2022/exact_matches_split/")
-    # base_dir = "../../data/libraries_and_models/gnps_01_11_2022/20_fold_splits/"
-    # annotated_spectra = load_matchms_spectrum_objects_from_file(os.path.join(base_dir, "annotated_spectra.pickle"))
-    # split_k_fold_cross_validation_exact_match_test_set(annotated_spectra,
-    #                                                    k=20,
-    #                                                    output_folder="../../data/libraries_and_models/gnps_01_11_2022/exact_matches_split/")
-
-    # save_pickled_file(all_spectra, os.path.join(base_dir, "annotated_spectra.pickle"))
+    generate_exact_matches_test_results(ms2library,
+                                        annotated_training_spectra,
+                                        test_spectra,
+                                        test_results_folder)
