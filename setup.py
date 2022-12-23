017481196553430804a7073e6f26f22544e3d8a9
Jonge <niek.dejonge@wur.nl>
2022-12-23 15:34:41 +0100

    set tensorflow to 2.8 to prevent printing progress bars

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setup(
         "numpy",
         "spec2vec>=0.6.0",
         "h5py<3.0.0",
-        "tensorflow-macos<=2.10.1;platform_machine=='arm64'",
-        "tensorflow-metal<=2.10.1;platform_machine=='arm64'",
-        "tensorflow<=2.10.1;platform_machine!='arm64'",
+        "tensorflow-macos<2.9;platform_machine=='arm64'", #Add for Macos M1 chip compatability
+        "tensorflow-metal<2.9;platform_machine=='arm64'",
+        "tensorflow<2.9;platform_machine!='arm64'", #tensofrlow <2.9 for change in error bar plotting
         "scikit-learn==0.24.2",
         "ms2deepscore",
         "gensim>=4.0.0",
