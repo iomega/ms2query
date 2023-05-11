a45d4112a78a6ba4e0a898d1c972740960cad7a3
niekdejonge <niekdejonge@gmail.com>
2023-05-11 17:15:43 +0200

    Fix tests by changing expected outcome to a,b,c,d,e,f,g

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,7 @@ import pytest
 from matchms import Spectrum
 from ms2query.utils import (add_unknown_charges_to_spectra,
                             load_matchms_spectrum_objects_from_file,
-                            get_classifier_from_csv_file, load_pickled_file,
+                            load_pickled_file,
                             convert_to_onnx_model, load_ms2query_model, predict_onnx_model)
 
 
@@ -63,71 +63,6 @@ def test_add_unknown_charges_to_spectra():
         assert spectrum.get("charge") == 2, "The charge is expected to be 2"
 
 
-def create_test_classifier_csv_file(tmp_path):
-    file_location = os.path.join(tmp_path, "test_csv_file")
-    with open(file_location, "w") as test_file:
-        test_file.write(
-            "inchi_key	smiles	cf_kingdom	cf_superclass	cf_class	cf_subclass	cf_direct_parent	npc_class_results	npc_superclass_results	npc_pathway_results	npc_isglycoside\n"
-            "IYDKWWDUBYWQGF-NNAZGLEUSA-N	CC(C)CC1NC(=O)C(C)NC(=O)C(=C)N(C)C(=O)CCC(NC(=O)C(C)C(NC(=O)C(CCCNC(N)=N)NC(=O)C(C)C(NC1=O)C(O)=O)\C=C\C(\C)=C\C(C)C(O)Cc1ccccc1)C(O)=O	Organic compounds	Organic acids and derivatives	Peptidomimetics	Hybrid peptides	Hybrid peptides	Cyclic peptides; Microcystins	Oligopeptides	Amino acids and Peptides	0\n"
-            "KNGPFNUOXXLKCN-ZNCJFREWSA-N	CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O	Organic compounds	Organic acids and derivatives	Peptidomimetics	Depsipeptides	Cyclic depsipeptides	Cyclic peptides	Oligopeptides	Amino acids and Peptides	0\n"
-            "HKSZLNNOFSGOKW-NSCMQRKRSA-N	CCCCCCC[C@@H](C/C=C/CCC(=O)NC/C(=C/Cl)/[C@@]12[C@@H](O1)[C@H](CCC2=O)O)OC	Organic compounds	Organoheterocyclic compounds	Oxepanes		Oxepanes	Lipopeptides	Oligopeptides	Amino acids and Peptides	0")
-    return file_location
-
-
-def test_get_classifier_from_csv_file(tmp_path):
-    create_test_classifier_csv_file(tmp_path)
-    output = get_classifier_from_csv_file(
-        os.path.join(tmp_path, "test_csv_file"),
-        ["IYDKWWDUBYWQGF", "BBBBBBBBBBBBBB"])
-    assert isinstance(output, pd.DataFrame), "Expected pandas DataFrame"
-    expected_columns = ["inchikey", "cf_kingdom", "cf_superclass",
-                        "cf_class", "cf_subclass", "cf_direct_parent",
-                        "npc_class_results", "npc_superclass_results",
-                        "npc_pathway_results"]
-    assert all([column_name in output.columns
-                for column_name in expected_columns]), \
-        "Expected different column names"
-    assert output.shape == (2, len(expected_columns)), "Expected different shape of dataframe"
-    assert "IYDKWWDUBYWQGF" in list(output["inchikey"]), \
-        "IYDKWWDUBYWQGF was expected in column inchikey"
-    assert "BBBBBBBBBBBBBB" in list(output["inchikey"]), \
-        "BBBBBBBBBBBBBB was expected in column inchikey"
-    pd.testing.assert_frame_equal(
-        output[output["inchikey"] == "BBBBBBBBBBBBBB"].reset_index(drop=True),
-        pd.DataFrame(np.array(
-            [["BBBBBBBBBBBBBB"] + [np.nan] * (len(expected_columns)-1)]), columns=expected_columns)
-    )
-    pd.testing.assert_frame_equal(
-        output[output["inchikey"] == "IYDKWWDUBYWQGF"].reset_index(drop=True),
-        pd.DataFrame(np.array(["IYDKWWDUBYWQGF",
-                               "Organic compounds",
-                               "Organic acids and derivatives",
-                               "Peptidomimetics", "Hybrid peptides",
-                               "Hybrid peptides",
-                               "Cyclic peptides; Microcystins",
-                               "Oligopeptides",
-                               "Amino acids and Peptides"]).reshape(-1, len(expected_columns)),
-                     columns=expected_columns)
-    )
-
-
-# def test_get_classifier_from_csv_file_empty(tmp_path):
-#     """Test if empty dataframe is returned, when no inchikeys are given"""
-#     create_test_classifier_csv_file(tmp_path)
-#     output = get_classifier_from_csv_file(
-#         os.path.join(tmp_path, "test_csv_file"),
-#         [])
-#     assert isinstance(output, pd.DataFrame), "Expected pandas DataFrame"
-#     expected_columns = ["inchikey", "smiles", "cf_kingdom", "cf_superclass",
-#                         "cf_class", "cf_subclass", "cf_direct_parent",
-#                         "npc_class_results", "npc_superclass_results",
-#                         "npc_pathway_results"]
-#     # Test if empty dataset with correct columns is created
-#     pd.testing.assert_frame_equal(output,
-#                                   pd.DataFrame(columns=expected_columns),
-#                                   check_dtype=False)
-
-
 def test_save_as_onnx_model(tmp_path):
     path_to_test_dir = os.path.join(
         os.path.split(os.path.dirname(__file__))[0],
@@ -151,8 +86,8 @@ def check_correct_results_csv_file(dataframe_found: pd.DataFrame,
                                    nr_of_rows_to_check=2):
     # Define expected results
     csv_format_expected_results ="""query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrumid,analog_compound_name,charge,s2v_score,ms2ds_score,retention_time,retention_index,smiles,cf_kingdom,cf_superclass,cf_class,cf_subclass,cf_direct_parent,npc_class_results,npc_superclass_results,npc_pathway_results\n
-        1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001760,Hoiamide B,1,0.9996,0.9232,,,CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O,Organic compounds,Organic acids and derivatives,Peptidomimetics,Depsipeptides,Cyclic depsipeptides,Cyclic peptides,Oligopeptides,Amino acids and Peptides\n
-        2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001761,Halovir A,0,0.9621,0.4600,,,CCCCCCCCCCCCCC(=O)NC(C)(C)C(=O)N1C[C@H](O)C[C@H]1C(=O)NC(CC(C)C)C(=O)N[C@@H](C(C)C)C(=O)N[C@@H](CCC(N)=O)C(=O)N[C@H](CO)CC(C)C,nan,nan,nan,nan,nan,nan,nan,nan\n"""
+        1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001760,Hoiamide B,1,0.9996,0.9232,,,CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O,b,c,d,e,f,g,h,i\n
+        2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001761,Halovir A,0,0.9621,0.4600,,,CCCCCCCCCCCCCC(=O)NC(C)(C)C(=O)N1C[C@H](O)C[C@H]1C(=O)NC(CC(C)C)C(=O)N[C@@H](C(C)C)C(=O)N[C@@H](CCC(N)=O)C(=O)N[C@H](CO)CC(C)C,b,c,d,e,f,g,h,i\n"""
     dataframe_expected_results = pd.read_csv(StringIO(csv_format_expected_results), sep=",", header=0)
 
     # convert csv rows to dataframe
