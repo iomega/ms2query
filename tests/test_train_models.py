f243c0941f9a4f4a73d7ee558e46d80f5c70183a
niekdejonge <niekdejonge@gmail.com>
2023-05-18 11:11:18 +0200

    Add test_train_models.py test again

--- tests/test_train_models.py
+++ tests/test_train_models.py
@@ -3,17 +3,18 @@ import pytest
 from ms2query.create_new_library.train_models import clean_and_train_models
 from ms2query.ms2library import create_library_object_from_one_dir, MS2Library
 
-# @pytest.mark.integration
-# def test_train_all_models(path_to_general_test_files, tmp_path):
-#     path_to_test_spectra = os.path.join(path_to_general_test_files, "1000_test_spectra.pickle")
-#     models_folder = os.path.join(tmp_path, "models")
-#     clean_and_train_models(path_to_test_spectra,
-#                            "positive",
-#                            models_folder,
-#                            {"ms2ds_fraction_validation_spectra": 2,
-#                             "ms2ds_epochs": 2,
-#                             "spec2vec_iterations": 2,
-#                             "ms2query_fraction_for_making_pairs": 400}
-#                            )
-#     ms2library = create_library_object_from_one_dir(models_folder)
-#     assert isinstance(ms2library, MS2Library)
+
+@pytest.mark.integration
+def test_train_all_models(path_to_general_test_files, tmp_path):
+    path_to_test_spectra = os.path.join(path_to_general_test_files, "1000_test_spectra.pickle")
+    models_folder = os.path.join(tmp_path, "models")
+    clean_and_train_models(path_to_test_spectra,
+                           "positive",
+                           models_folder,
+                           {"ms2ds_fraction_validation_spectra": 2,
+                            "ms2ds_epochs": 2,
+                            "spec2vec_iterations": 2,
+                            "ms2query_fraction_for_making_pairs": 400}
+                           )
+    ms2library = create_library_object_from_one_dir(models_folder)
+    assert isinstance(ms2library, MS2Library)
