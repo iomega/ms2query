8c8c587c42ec48bbad205f9d982b0956d9d9b38b
niekdejonge <niekdejonge@gmail.com>
2023-05-23 16:32:58 +0200

    Add test train_ms2query_model

--- tests/test_train_ms2query_model.py
+++ tests/test_train_ms2query_model.py
@@ -3,11 +3,12 @@ import pytest
 import sys
 import pandas as pd
 from ms2query.create_new_library.train_ms2query_model import \
-    DataCollectorForTraining, calculate_tanimoto_scores_with_library, train_random_forest
+    DataCollectorForTraining, calculate_tanimoto_scores_with_library, train_random_forest, train_ms2query_model
 from ms2query.utils import load_pickled_file, load_matchms_spectrum_objects_from_file, convert_to_onnx_model
 from onnxruntime import InferenceSession
 from ms2query.utils import predict_onnx_model
 from ms2query.ms2library import MS2Library
+
 
 if sys.version_info < (3, 8):
     pass
@@ -75,7 +76,7 @@ def test_calculate_all_tanimoto_scores(tmp_path, ms2library, query_spectra):
     pd.testing.assert_frame_equal(result, expected_result, check_dtype=False)
 
 
-def test_train_ms2query_model():
+def test_train_random_forest():
     training_scores, training_labels = load_pickled_file(os.path.join(
         os.path.split(os.path.dirname(__file__))[0],
         "tests/test_files/test_files_train_ms2query_nn",
@@ -84,3 +85,17 @@ def test_train_ms2query_model():
     onnx_model = convert_to_onnx_model(ms2query_model)
     onnx_model_session = InferenceSession(onnx_model.SerializeToString())
     predictions = predict_onnx_model(onnx_model_session, training_scores.values)
+
+
+@pytest.mark.integration
+def test_train_ms2query_model(path_to_general_test_files, tmp_path, hundred_test_spectra):
+    models_folder = os.path.join(tmp_path, "models")
+    ms2query_model = train_ms2query_model(
+        training_spectra=hundred_test_spectra,
+        library_files_folder=models_folder,
+        ms2ds_model_file_name=os.path.join(path_to_general_test_files,
+                                           "ms2ds_siamese_210301_5000_500_400.hdf5"),
+        s2v_model_file_name=os.path.join(path_to_general_test_files,
+                                         "100_test_spectra_s2v_model.model"),
+        fraction_for_training=10
+    )
