e925707363fa80caaad6344fecfddd7b86fc9855
niekdejonge <niekdejonge@gmail.com>
2023-11-22 17:14:31 +0100

    Add general_test_files to path_to_tests_dir in ms2library

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,24 +26,24 @@ def ms2library() -> MS2Library:
     """Returns file names of the files needed to create MS2Library object"""
     path_to_tests_dir = os.path.join(
         os.path.split(os.path.dirname(__file__))[0],
-        'tests/test_files/')
+        'tests/test_files/general_test_files/')
     sqlite_file_loc = os.path.join(
         path_to_tests_dir,
-        "general_test_files/100_test_spectra.sqlite")
+        "100_test_spectra.sqlite")
     spec2vec_model_file_loc = os.path.join(
         path_to_tests_dir,
-        "general_test_files/100_test_spectra_s2v_model.model")
+        "100_test_spectra_s2v_model.model")
     s2v_pickled_embeddings_file = os.path.join(
         path_to_tests_dir,
-        "general_test_files/100_test_spectra_s2v_embeddings.pickle")
+        "100_test_spectra_s2v_embeddings.pickle")
     ms2ds_model_file_name = os.path.join(
         path_to_tests_dir,
-        "general_test_files/ms2ds_siamese_210301_5000_500_400.hdf5")
+        "ms2ds_siamese_210301_5000_500_400.hdf5")
     ms2ds_embeddings_file_name = os.path.join(
         path_to_tests_dir,
-        "general_test_files/100_test_spectra_ms2ds_embeddings.pickle")
+        "100_test_spectra_ms2ds_embeddings.pickle")
     ms2q_model_file_name = os.path.join(path_to_tests_dir,
-        "general_test_files", "test_ms2q_rf_model.onnx")
+        "test_ms2q_rf_model.onnx")
     ms2library = MS2Library(sqlite_file_loc, spec2vec_model_file_loc, ms2ds_model_file_name,
                             s2v_pickled_embeddings_file, ms2ds_embeddings_file_name, ms2q_model_file_name)
     return ms2library
