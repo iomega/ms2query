4566a3e08d54dd04282739200d48bac13fc3559d
niekdejonge <niekdejonge@gmail.com>
2023-11-22 17:13:13 +0100

    Remove path_to_test_files_fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,13 +16,8 @@ def path_to_general_test_files() -> str:
 
 
 @pytest.fixture(scope="package")
-def path_to_test_files():
-    return os.path.join(os.path.split(os.path.dirname(__file__))[0], 'tests/test_files')
-
-
-@pytest.fixture(scope="package")
-def sqlite_library(path_to_test_files):
-    path_to_library = os.path.join(path_to_test_files, "general_test_files", "100_test_spectra.sqlite")
+def sqlite_library(path_to_general_test_files):
+    path_to_library = os.path.join(path_to_general_test_files, "100_test_spectra.sqlite")
     return SqliteLibrary(path_to_library)
 
 
