a1367da7f0aafb4b9460e4fd45b1197b5298f536
niekdejonge <niekdejonge@gmail.com>
2023-11-23 23:46:59 +0100

    Add get_ms2deepscore_embeddings (from sqlite) to the test create sqlite file with embeddings

--- tests/test_library_files_creator.py
+++ tests/test_library_files_creator.py
@@ -4,7 +4,7 @@ import pytest
 from ms2query.clean_and_filter_spectra import normalize_and_filter_peaks
 from ms2query.create_new_library.library_files_creator import \
     LibraryFilesCreator
-
+from ms2query.query_from_sqlite_database import SqliteLibrary
 
 def test_give_already_used_file_name(tmp_path, path_to_general_test_files, hundred_test_spectra):
     already_existing_file = os.path.join(tmp_path, "ms2query_library.sqlite")
@@ -44,10 +44,11 @@ def test_create_s2v_embeddings(tmp_path, path_to_general_test_files, hundred_tes
                                   atol=1e-5)
 
 
-def test_create_sqlite_file_with_embeddings(hundred_test_spectra, path_to_general_test_files):
+def test_create_sqlite_file_with_embeddings(tmp_path, hundred_test_spectra, path_to_general_test_files, expected_ms2ds_embeddings):
+    library_spectra = [normalize_and_filter_peaks(s) for s in hundred_test_spectra[:20] if s is not None]
     test_create_files = LibraryFilesCreator(
-        hundred_test_spectra[:20],
-        output_directory=os.path.join(path_to_general_test_files),
+        library_spectra,
+        output_directory=os.path.join(tmp_path),
         add_compound_classes=False,
         ms2ds_model_file_name=os.path.join(path_to_general_test_files,
                                            'ms2ds_siamese_210301_5000_500_400.hdf5'),
@@ -55,3 +56,14 @@ def test_create_sqlite_file_with_embeddings(hundred_test_spectra, path_to_genera
                                          "100_test_spectra_s2v_model.model")
     )
     test_create_files.create_sqlite_file()
+    sqlite_library = SqliteLibrary(os.path.join(tmp_path, "ms2query_library.sqlite"))
+    ms2deepscore_embeddings = sqlite_library.get_ms2deepscore_embeddings()
+    ms2deepscore_embeddings.columns = pd.RangeIndex(start=0,
+                                                    stop=len(ms2deepscore_embeddings.columns),
+                                                    step=1)
+
+    pd.testing.assert_frame_equal(ms2deepscore_embeddings,
+                                  expected_ms2ds_embeddings.loc[:19],
+                                  check_exact=False,
+                                  atol=1e-5,
+                                  check_names=False)
