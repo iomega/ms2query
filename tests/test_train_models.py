b7f50b763b39ce8f180822cfde6d9816ef7b69e7
niekdejonge <niekdejonge@gmail.com>
2024-06-24 13:12:47 +0200

    Update test train_model to use settingsms2deepscore

--- tests/test_train_models.py
+++ tests/test_train_models.py
@@ -1,5 +1,8 @@
 import os
+import numpy as np
 import pytest
+from ms2deepscore import SettingsMS2Deepscore
+
 from ms2query.create_new_library.train_models import clean_and_train_models
 from ms2query.ms2library import MS2Library, create_library_object_from_one_dir
 
@@ -12,7 +15,14 @@ def test_train_all_models(path_to_general_test_files, tmp_path):
                            "negative",
                            models_folder,
                            {"ms2ds_fraction_validation_spectra": 2,
-                            "ms2ds_epochs": 2,
+                            "ms2ds_training_settings": SettingsMS2Deepscore(
+                                mz_bin_width=1.0,
+                                epochs=2,
+                                base_dims=(100, 100),
+                                embedding_dim=50,
+                                same_prob_bins=np.array([(0, 0.5), (0.5, 1.0)]),
+                                average_pairs_per_bin=2,
+                                batch_size=2),
                             "spec2vec_iterations": 2,
                             "ms2query_fraction_for_making_pairs": 400,
                             "add_compound_classes": False}
